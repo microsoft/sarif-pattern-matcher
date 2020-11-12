2a1af3546082e5039175d5c3135e01b213d8dd2b
Eddy Nakamura <eddynaka@gmail.com>
2020-11-12 19:04:22 -0300

    BannedApi will move content to .spam in root source folder (#9)
    * BannedApi will move content to .spam in root source folder
    
    * fixing spacing
    
    * moving span to sln instead of root source

--- Src/Plugins/BannedApi/tools/Init.ps1
+++
@@ -1,6 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$file1 = $project.ProjectItems.Item("BannedApiRegex.json")
-
-$copyToOutput1 = $file1.Properties.Item("CopyToOutputDirectory")
-$copyToOutput1.Value = 1 
