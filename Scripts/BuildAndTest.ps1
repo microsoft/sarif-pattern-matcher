c71a0a5f360d9d2d70864fb23c2ac29502650c4a
Eddy Nakamura <ednakamu@microsoft.com>
2020-12-02 22:49:57 -0300

    Fixing configuration issue

--- Scripts/BuildAndTest.ps1
+++ Scripts/BuildAndTest.ps1
@@ -1,3 +1,39 @@
+<#
+.SYNOPSIS
+    Build, test, and package the sarif-pattern-matcher.
+.DESCRIPTION
+    Builds the sarif-pattern-matcher for multiple target frameworks, runs the tests, and creates
+    NuGet packages.
+.PARAMETER Configuration
+    The build configuration: Release or Debug. Default=Release
+.PARAMETER NoBuild
+    Do not build.
+.PARAMETER NoTest
+    Do not run tests.
+.PARAMETER NoPackage
+    Do not create NuGet packages.
+.PARAMETER NoFormat
+    Do not format files based on dotnet-format tool
+#>
+
+[CmdletBinding()]
+param(
+    [string]
+    [ValidateSet("Debug", "Release")]
+    $Configuration="Release",
+
+    [switch]
+    $NoBuild,
+
+    [switch]
+    $NoTest,
+
+    [switch]
+    $NoPackage,
+    
+    [switch]
+    $NoFormat
+)
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
@@ -29,34 +65,43 @@ If (Test-Path "..\bld") {
     rd /s /q ..\bld
 }
 
-Write-Information "Building RE2.Native.sln (MSBuild)..."
-MSBuild "$RepoRoot\Src\RE2.Native.sln" /p:Configuration=Release /p:Platform="Any CPU"
+if (-not $NoBuild) {
+    Write-Information "Building RE2.Native.sln (MSBuild)..."
+    MSBuild "$RepoRoot\Src\RE2.Native.sln" /p:Configuration=$Configuration /p:Platform="Any CPU"
 
-Write-Information "Building Sarif.Sdk"
-& $RepoRoot\Src\sarif-sdk\BuildAndTest.cmd -NoTest -NoPublish
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Build of sarif.sdk failed."
+    Write-Information "Building Sarif.Sdk"
+    & $RepoRoot\Src\sarif-sdk\BuildAndTest.cmd -NoTest -NoPublish -NoFormat -Configuration $Configuration
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Build of sarif.sdk failed."
+    }
+
+    Write-Information "Building SarifPatternMatcher.sln (dotnet)..."
+    dotnet build $RepoRoot\Src\SarifPatternMatcher.sln -c $Configuration -p:Deterministic=true
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Build of SarifPatternMatcher failed."
+    }
 }
 
-Write-Information "Building SarifPatternMatcher.sln (dotnet)..."
-dotnet build $RepoRoot\Src\SarifPatternMatcher.sln -c Release -p:Deterministic=true
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Build of SarifPatternMatcher failed."
+if (-not $NoPackage) {
+    Write-Information "Packing SarifPatternMatcher.sln (dotnet)..."
+    dotnet pack $RepoRoot\Src\SarifPatternMatcher.sln -c $Configuration --no-build
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Pack of SarifPatternMatcher failed."
+    }
 }
 
-Write-Information "Packing SarifPatternMatcher.sln (dotnet)..."
-dotnet pack $RepoRoot\Src\SarifPatternMatcher.sln -c Release --no-build
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Pack of SarifPatternMatcher failed."
+if (-not $NoTest) {
+    Write-Information "Running tests..."
+    dotnet test $RepoRoot\Src\SarifPatternMatcher.sln -c $Configuration --no-build --collect:"XPlat Code Coverage"
+    if ($LASTEXITCODE -ne 0) {
+        Exit-WithFailureMessage $ScriptName "Test of SarifPatternMatcher failed."
+    }
 }
 
-Write-Information "Running tests..."
-dotnet test $RepoRoot\Src\SarifPatternMatcher.sln -c Release --no-build --collect:"XPlat Code Coverage"
-if ($LASTEXITCODE -ne 0) {
-    Exit-WithFailureMessage $ScriptName "Test of SarifPatternMatcher failed."
-}
 
-dotnet tool update --global dotnet-format --version 4.1.131201
-dotnet-format --folder
+if (-not $NoFormat) {
+    dotnet tool update --global dotnet-format --version 4.1.131201
+    dotnet-format --folder --exclude .\src\sarif-sdk\
+}
 
 Write-Information "$ScriptName SUCCEEDED."
