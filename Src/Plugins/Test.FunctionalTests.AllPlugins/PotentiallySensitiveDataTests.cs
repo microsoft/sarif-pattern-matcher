9a938a8e6c92e248d6f5e9e39e0681ffbc78a3f6
Michael Fanning <mikefan@microsoft.com>
2020-12-15 14:38:29 -0800

    Expand end-to-end test capability.

---
+++ Src/Plugins/Test.FunctionalTests.AllPlugins/PotentiallySensitiveDataTests.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.SarifPatternMatcher.Test.FunctionalTests.AllPlugins
+{
+    public class PotentiallySensitiveDataTests : EndToEndTests
+    {
+        public PotentiallySensitiveDataTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
+
+        [Fact]
+        public void EndToEndFunctionalTests_PotentiallySensitiveData()
+            => RunAllTests();
+    }
+}
