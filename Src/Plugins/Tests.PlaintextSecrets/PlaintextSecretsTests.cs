22b8626fe9a5514bc208ca70003a5ed972138cb0
Eddy Nakamura <eddynaka@gmail.com>
2020-12-17 14:07:04 -0300

    Refactor plugins tests (#33)
    * Refactor plugins tests
    
    * fixing tests
    
    * updating files after merge
    
    * simplifying test csproj
    
    * adding sensitivefiles tests

--- Src/Plugins/Tests.PlaintextSecrets/PlaintextSecretsTests.cs
+++ Src/Plugins/Tests.PlaintextSecrets/PlaintextSecretsTests.cs
@@ -1,19 +1,22 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
+using Microsoft.CodeAnalysis.SarifPatternMatcher.Test.AllPlugins;
 
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Microsoft.CodeAnalysis.SarifPatternMatcher.Test.FunctionalTests.AllPlugins
+namespace Microsoft.CodeAnalysis.SarifPatternMatcher.Tests.PlaintextSecrets
 {
     public class PlaintextSecretsTests : EndToEndTests
     {
+        protected override string TestLogResourceNameRoot
+            => "Microsoft.CodeAnalysis.SarifPatternMatcher.Tests.PlaintextSecrets.TestData.PlaintextSecrets";
+
         public PlaintextSecretsTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
 
         [Fact]
-        public void EndToEndFunctionalTests_PlaintextSecrets()
+        public void EndToEndFunctionalTests()
             => RunAllTests();
     }
 }
--- Src/Plugins/Tests.PotentiallySensitiveData/PotentiallySensitiveDataTests.cs
+++ Src/Plugins/Tests.PotentiallySensitiveData/PotentiallySensitiveDataTests.cs
@@ -1,19 +1,22 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
+using Microsoft.CodeAnalysis.SarifPatternMatcher.Test.AllPlugins;
 
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Microsoft.CodeAnalysis.SarifPatternMatcher.Test.FunctionalTests.AllPlugins
+namespace Microsoft.CodeAnalysis.SarifPatternMatcher.Tests.PotentiallySensitiveData
 {
     public class PotentiallySensitiveDataTests : EndToEndTests
     {
+        protected override string TestLogResourceNameRoot
+            => "Microsoft.CodeAnalysis.SarifPatternMatcher.Tests.PotentiallySensitiveData.TestData.PotentiallySensitiveData";
+
         public PotentiallySensitiveDataTests(ITestOutputHelper outputHelper) : base(outputHelper) { }
 
         [Fact]
-        public void EndToEndFunctionalTests_PotentiallySensitiveData()
+        public void EndToEndFunctionalTests()
             => RunAllTests();
     }
 }
