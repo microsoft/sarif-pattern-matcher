acbffdbefc1c5d5b0d5dcf1fbbe5003416d45909
Michael C. Fanning <michael.fanning@microsoft.com>
2020-12-17 04:08:08 -0800

    Test base64 decoding. Return no results when validator returns 'no match'. (#34)
    * Test base64 decoding. Return no results when validator indicates a non-match.
    
    * Fix PAT rule to include non-encoded match length (and update to new property name).
    
    * Trivial clean-up after first review.
    
    * Resolve exception in skimmer
    
    * fixing spacing
    
    * changing from tostring to nameof
    
    Co-authored-by: Eddy Nakamura <ednakamu@microsoft.com>

---
+++ Src/Test.UnitTests.SarifPatternMatcher/SearchSkimmerTests.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FluentAssertions;
+
+using Microsoft.CodeAnalysis.Sarif;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.SarifPatternMatcher.SarifPatternMatcher
+{
+    public class SearchSkimmerTests
+    {
+        private static MatchExpression CreateGuidDetectingMatchExpression()
+        {
+            const string guidRegexText = "(?i)[0-9a-f]{8}[-]?([0-9a-f]{4}[-]?){3}[0-9a-f]{12}";
+
+            return new MatchExpression
+            {
+                MatchLengthToDecode = Guid.NewGuid().ToString().Length,
+                ContentsRegex = guidRegexText,
+            };
+        }
+
+        [Fact]
+        public void SearchSkimmer_DetectsBase64EncodedPattern()
+        {
+            MatchExpression expr = CreateGuidDetectingMatchExpression();
+            SearchDefinition definition = CreateDefaultSearchDefinition(expr);
+
+            string originalMessage = definition.Message;
+
+            // We inject the well-known encoding name that reports with
+            // 'plaintext' or 'base64-encoded' depending on how a match
+            // was made.
+            definition.Message = $"{{0:encoding}}:{definition.Message}";
+
+            string scanTargetContents = definition.Id;
+
+            byte[] bytes = Encoding.UTF8.GetBytes(scanTargetContents);
+            string base64Encoded = Convert.ToBase64String(bytes);
+
+            var logger = new TestLogger();
+
+            var context = new AnalyzeContext
+            {
+                TargetUri = new Uri($"file:///c:/{definition.Name}.{definition.DefaultNameRegex}"),
+                FileContents = base64Encoded,
+                Logger = logger
+            };
+
+            SearchSkimmer skimmer = CreateSkimmer(definition);
+            skimmer.Analyze(context);
+
+            // Analyzing base64-encoded values with MatchLengthToDecode > 0 succeeds
+            logger.Results.Count.Should().Be(1);
+            logger.Results[0].RuleId.Should().Be(definition.Id);
+            logger.Results[0].Level.Should().Be(definition.Level);
+            logger.Results[0].GetMessageText(skimmer).Should().Be($"base64-encoded:{originalMessage}");
+
+            // Analyzing base64-encoded values with MatchLengthToDecode == 0 fails
+            definition.MatchExpressions[0].MatchLengthToDecode = 0;
+
+            logger.Results.Clear();
+            skimmer = CreateSkimmer(definition);
+            skimmer.Analyze(context);
+
+            logger.Results.Count.Should().Be(0);
+
+            // Analyzing plaintext values with MatchLengthToDecode > 0 succeeds
+            context.FileContents = scanTargetContents;
+
+            logger.Results.Clear();
+            skimmer = CreateSkimmer(definition);
+            skimmer.Analyze(context);
+
+            // But we should see a change in encoding information in message.
+            logger.Results.Count.Should().Be(1);
+            logger.Results[0].RuleId.Should().Be(definition.Id);
+            logger.Results[0].Level.Should().Be(definition.Level);
+            logger.Results[0].GetMessageText(skimmer).Should().Be($"plaintext:{originalMessage}");
+        }
+
+        private SearchSkimmer CreateSkimmer(SearchDefinition definition, IRegex engine = null, ValidatorsCache validators = null)
+        {
+            return new SearchSkimmer(
+                engine: engine ?? RE2Regex.Instance,
+                validators: validators,
+                definition);
+        }
+
+        private SearchDefinition CreateDefaultSearchDefinition(MatchExpression matchExpression)
+        {
+            return new SearchDefinition()
+            {
+                DefaultNameRegex = Guid.NewGuid().ToString(),
+                Description = Guid.NewGuid().ToString(),
+                Id = Guid.NewGuid().ToString(),
+                Level = FailureLevel.Error,
+                MatchExpressions = new List<MatchExpression> { matchExpression },
+                Message = Guid.NewGuid().ToString(),
+                Name = Guid.NewGuid().ToString(),
+            };
+        }
+    }
+}
