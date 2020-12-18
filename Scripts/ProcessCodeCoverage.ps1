ceeed1f3432827bdb83225a5d299e2086210d56a
Eddy Nakamura <ednakamu@microsoft.com>
2020-12-18 15:55:50 -0300

    Fixing codecoverage

---
+++ Scripts/ProcessCodeCoverage.ps1
@@ -0,0 +1,10 @@
+nuget install Microsoft.CodeCoverage -version 16.8.3
+
+$files = Get-ChildItem "bld\TestResults" -Filter "*.coverage" -Recurse
+
+foreach ($file in $files)
+{
+    $command = 'microsoft.codecoverage.16.8.3\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    Write-Host $command
+    Invoke-Expression $command
+}
