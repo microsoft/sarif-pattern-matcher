9e4dbcdc0b0cd5f05b5abd9a703a572a866e7a7f
Eddy Nakamura <ednakamu@microsoft.com>
2021-01-09 14:24:48 -0300

    Adding dotnet restore only

--- Scripts/BuildAndTest.ps1
+++ Scripts/BuildAndTest.ps1
@@ -63,15 +63,12 @@ If (Test-Path "..\bld") {
 if (-not $NoBuild) {
     Write-Information "Building RE2.Native.sln (MSBuild)..."
     MSBuild "$RepoRoot\Src\RE2.Native.sln" /p:Configuration=$Configuration /p:Platform="Any CPU"
-
-    Write-Information "Building Sarif.Sdk"	
-    & $RepoRoot\Src\sarif-sdk\BuildAndTest.cmd -NoTest -NoPublish -NoFormat -Configuration $Configuration	
-    if ($LASTEXITCODE -ne 0) {	
-        Exit-WithFailureMessage $ScriptName "Build of sarif.sdk failed."	
-    }
+    
+    Write-Information "Restoring SarifPatternMatcher.sln (dotnet)..."
+    dotnet restore $RepoRoot\Src\SarifPatternMatcher.sln
 
     Write-Information "Building SarifPatternMatcher.sln (dotnet)..."
-    dotnet build $RepoRoot\Src\SarifPatternMatcher.sln -c $Configuration -p:Deterministic=true
+    dotnet build $RepoRoot\Src\SarifPatternMatcher.sln -c $Configuration -p:Deterministic=true --no-restore
     if ($LASTEXITCODE -ne 0) {
         Exit-WithFailureMessage $ScriptName "Build of SarifPatternMatcher failed."
     }
