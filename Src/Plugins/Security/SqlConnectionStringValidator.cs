34fc29badfb6326f63e379720406e81830069195
Eddy Nakamura <ednakamu@microsoft.com>
2021-02-01 19:15:48 -0300

    SqlConnectionString validator

---
+++ Src/Plugins/Security/SqlConnectionStringValidator.cs
@@ -0,0 +1,131 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+using Microsoft.RE2.Managed;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    internal class SqlConnectionStringValidator : ValidatorBase
+    {
+        internal static SqlConnectionStringValidator Instance;
+        internal static IRegex RegexEngine;
+
+        private const string ClientIPExpression = @"Client with IP address '[^']+' is not allowed to access the server.";
+
+        static SqlConnectionStringValidator()
+        {
+            Instance = new SqlConnectionStringValidator();
+            RegexEngine = RE2Regex.Instance;
+
+            // We perform this work in order to force caching of these
+            // expressions (an operation which otherwise can cause
+            // threading problems).
+            RegexEngine.Match(string.Empty, ClientIPExpression);
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return ValidatorBase.IsValidStatic(Instance,
+                                               ref matchedPattern,
+                                               ref groups,
+                                               ref failureLevel,
+                                               ref fingerprint,
+                                               ref message);
+        }
+
+        public static string IsValidDynamic(ref string fingerprint, ref string message)
+        {
+            return ValidatorBase.IsValidDynamic(Instance,
+                                                ref fingerprint,
+                                                ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetValue("host", out string host) ||
+                !groups.TryGetValue("account", out string account) ||
+                !groups.TryGetValue("password", out string password))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            // SQL server name can't exceed this length. If we have, we likely
+            // are looking at a lengthy string is an indirect key to the
+            // actual SQL server name.
+            if (host.Length > 128)
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint()
+            {
+                Host = host,
+                Account = account,
+                Password = password,
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+
+        protected override string IsValidDynamicHelper(ref string fingerprintText,
+                                                       ref string message)
+        {
+            var fingerprint = new Fingerprint(fingerprintText);
+
+            string host = fingerprint.Host;
+            string account = fingerprint.Account;
+            string password = fingerprint.Password;
+
+            string connString =
+                $"Server=tcp:{host},1433;User ID={account};Password={password};" +
+                "Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            try
+            {
+                using var connection = new SqlConnection(connString);
+                connection.OpenAsync().GetAwaiter().GetResult();
+            }
+            catch (ArgumentException)
+            {
+                // This exception means that some illegal chars, etc.
+                // have snuck into the connection string
+                return nameof(ValidationState.NoMatch);
+            }
+            catch (Exception e)
+            {
+                if (e is SqlException sqlException)
+                {
+                    if (sqlException.ErrorCode == unchecked((int)0x80131904))
+                    {
+                        if (e.Message.Contains("Login failed for user"))
+                        {
+                            return ReturnUnauthorizedAccess(ref message, asset: host);
+                        }
+
+                        FlexMatch match = RegexEngine.Match(e.Message, ClientIPExpression);
+                        if (match.Success)
+                        {
+                            message = match.Value;
+                            return nameof(ValidationState.Unknown);
+                        }
+                    }
+                }
+
+                return ReturnUnhandledException(ref message, e, asset: host);
+            }
+
+            return ReturnAuthorizedAccess(ref message, asset: host);
+        }
+    }
+}
