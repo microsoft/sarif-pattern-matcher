74bff7b0c71abbe833c4d6f742b02762626172b4
Eddy Nakamura <eddynaka@gmail.com>
2021-02-01 19:22:50 -0300

    SqlConnectionString validator (#154)

---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_109.SqlConnectionString.ps1
@@ -0,0 +1,11 @@
+# This is a well-formed but invalid ADO sql connection string
+server=tcp:some-database-name.database.windows.net,1433;Initial Catalog=catalog-db;Persist Security Info=False;User ID=username;Password=password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+
+# This is a well-formed but invalid ODBC sql connection string
+jdbc:sqlserver://some-database-name.database.windows.net:1433;database=tse-internal-spam-testing;user=username@some-database-name;password=password;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+
+# This is a well-formed but invalid JDBC sql connection string
+Driver={ODBC Driver 13 for SQL Server};Server=tcp:some-database-name.database.windows.net,1433;Database=tse-internal-spam-testing;Uid=username;Pwd=password;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+
+# This is a well-formed but invalid PHP SQL connection string
+new PDO("sqlsrv:server = tcp:some-database-name.database.windows.net,1433; Database = some-database-name", "username", "password");
