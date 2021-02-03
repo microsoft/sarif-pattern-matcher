461326087262a0f8103cc86c1c6101af34f390ea
Eddy Nakamura <ednakamu@microsoft.com>
2021-02-03 10:22:39 -0300

    fixing ordering

--- Src/Plugins/ValidatorBase.cs
+++ Src/Plugins/ValidatorBase.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.RE2.Managed;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
+using Microsoft.RE2.Managed;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins
 {
