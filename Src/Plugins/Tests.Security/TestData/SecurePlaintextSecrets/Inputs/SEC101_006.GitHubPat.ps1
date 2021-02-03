8eb24dfc3890be1236108d5a0257e3e786e067ee
Eddy Nakamura <eddynaka@gmail.com>
2021-02-03 17:23:57 -0300

    Improving GitHub PAT search (#161)
    * Improving GitHub PAT search
    
    * addressing pr comments

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_006.GitHubPat.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_006.GitHubPat.ps1
@@ -11,5 +11,6 @@
 # Code style
 var myGitHubPersonalAccessToken = "ff34885a8624460a855540c6592698d2f1812843";
 
-
-
+# Link style
+# This should trigger, but it should report as NoMatch
+https://github.com/microsoft/sarif-pattern-matcher/commit/4196919fb12cc4aa8ec0159a746c1e03b4ffa89c
