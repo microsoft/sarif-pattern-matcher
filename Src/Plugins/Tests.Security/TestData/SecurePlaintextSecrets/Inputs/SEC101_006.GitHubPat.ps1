baef86524d2f4c119f5e9ae4c2921c8bb0c53abe
Eddy Nakamura <eddynaka@gmail.com>
2021-02-04 13:39:11 -0300

    Ignore pattern if contains tree (#164)
    * Ignore pattern if contains tree
    
    * adding more tests that the validator should ignore

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_006.GitHubPat.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_006.GitHubPat.ps1
@@ -15,3 +15,4 @@ var myGitHubPersonalAccessToken = "ff34885a8624460a855540c6592698d2f1812843";
 # This should trigger, but it should report as NoMatch
 https://github.com/microsoft/sarif-pattern-matcher/commit/4196919fb12cc4aa8ec0159a746c1e03b4ffa89c
 https://avatars0.githubusercontent.com/u/16199012?u=4196919fb12cc4aa8ec0159a746c1e03b4ffa89c&v=4
+https://github.com/microsoft/sarif-pattern-matcher/tree/7af498925c89a9e8c07fb8a65223e89d513d4eea
