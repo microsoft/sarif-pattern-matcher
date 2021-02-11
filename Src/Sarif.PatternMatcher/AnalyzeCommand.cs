9308f29e09555ed95a89353e0446af3ad95af012
Michael C. Fanning <michael.fanning@microsoft.com>
2021-02-11 14:29:48 -0800

    Remove FB rule. Improve shared string expansion assert. (#188)
    * Remove FB rule. Improve shared string expansion assert.
    
    * Update baselines.
    
    Co-authored-by: Eddy Nakamura <eddynaka@gmail.com>

--- Src/Sarif.PatternMatcher/AnalyzeCommand.cs
+++ Src/Sarif.PatternMatcher/AnalyzeCommand.cs
@@ -270,17 +270,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
             // We failed to expand a pattern that is entirely rendered
             // via a shared string.
             Debug.Assert(!text.StartsWith("$"),
-                         "Failed to expand shared string: '{text}'");
-
-            if (text.EndsWith("$")) { text = text.Substring(0, text.Length - 1); }
-            if (text.EndsWith("$]")) { text = text.Substring(0, text.Length - 2); }
-            if (text.EndsWith("$)")) { text = text.Substring(0, text.Length - 2); }
-
-            // We failed to expand a pattern within an expression. We
-            // trim a trailing '$' as it is commonly used to denote
-            // and end-of-line in search patterns.
-            Debug.Assert(!text.Substring(0, text.Length - 1).Contains("$"),
-                         $"Failed to expand shared string: {text}");
+                         $"Failed to expand shared string: '{text}'");
         }
 #endif
 
