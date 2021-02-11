96c7876e0681431935f86b415340832476a6da35
jameswinkler <77122232+jameswinkler@users.noreply.github.com>
2021-02-10 17:08:35 -0800

    Update a few regex to ignore order (#183)
    * Update facebook regex
    
    * Update github app credentials
    
    * Add aliases to SqlConnectionString
    
    * Order insensitivity for Sql connection string jdbc
    
    * Improve ending properties, make all example credentials unique
    
    * Rebase SqlConnectionString.sarif
    
    * Correct merge
    
    * Tweak regex

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_046.SqlConnectionString.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_046.SqlConnectionString.ps1
@@ -1,39 +1,43 @@
 # This is a well-formed but invalid ODBC sql connection string
-jdbc:sqlserver://some-database-name.database.windows.net:1433;database=tse-internal-spam-testing;user=username@some-database-name;password=password_1;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+jdbc:sqlserver://servername1.database.windows.net:1433;database=database1;user=username1@some-database-name;password=password1;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
 
 # This is a well-formed but invalid JDBC sql connection string
-Driver={ODBC Driver 13 for SQL Server};Server=tcp:some-database-name.database.windows.net,1433;Database=tse-internal-spam-testing;Uid=username;Pwd=password_2;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+Driver={ODBC Driver 13 for SQL Server};Server=tcp:servername2.database.windows.net,1433;Database=database2;Uid=username2;Pwd=password2;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
 
 # This is a well-formed but invalid PHP SQL connection string
-new PDO("sqlsrv:server = tcp:some-database-name.database.windows.net,1433; Database = some-database-name", "username", "password_3");
+new PDO("sqlsrv:server = tcp:servername3.database.windows.net,1433; Database = databse3", "username3", "password3");
 
 # Xml style
 <connectionStrings>
     <add name="YourConnectionStringKey" 
         providerName="System.Data.SqlClient"
-        connectionString="Data Source=server-name;Initial Catalog=catalog-db;User ID=username;Password=password_4;" />
+        connectionString="Data Source=servername4;Initial Catalog=database4;User ID=username4;Password=password4;" />
     <add name="YourConnectionStringKey" 
         providerName="System.Data.SqlClient"
-        connectionString="Password=password_5;User ID=username;Initial Catalog=catalog-db;Data Source=server-name;" />
+        connectionString="Password=password5;User ID=username5;Initial Catalog=database5;Data Source=database5;" />
     <add name="YourConnectionStringKey" 
         providerName="System.Data.SqlClient"
-        connectionString="Initial Catalog=catalog-db;Data Source=server-name;Password=password_6;User ID=username;" />
+        connectionString="Initial Catalog=database6;Data Source=servername6;Password=password6;User ID=username6;" />
 
-    # NoMatch
+    # NoMatch missing a password
     <add name="YourConnectionStringKey" 
         providerName="System.Data.SqlClient"
-        connectionString="Initial Catalog=catalog-db;Data Source=server-name;User ID=username;User ID=username;" />
+        connectionString="Initial Catalog=database7;Data Source=servername7;User ID=username7;User ID=username7;" />
 
 </connectionStrings>
 
 # AppSettings style
 <appSettings>
-  <add key="ConnectionString" value="Data Source=server-name;Initial Catalog=catalog-db;User ID=username;Password=password_7;" />
-  <add key="ConnectionString" value="Password=password_8;User ID=username;Initial Catalog=catalog-db;Data Source=server-name;" />
-  <add key="ConnectionString" value="Initial Catalog=catalog-db;Data Source=server-name;Password=password_9;User ID=username;" />
+  <add key="ConnectionString" value="Data Source=servername8;Initial Catalog=database8;User ID=username8;Password=password8;" />
+  <add key="ConnectionString" value="Password=password9;User ID=username9;Initial Catalog=database9;Data Source=servername9;" />
+  <add key="ConnectionString" value="Initial Catalog=database10;Data Source=servername10;Password=password10;User ID=username10;" />
 </appSettings>
 
 # Should be ignored, since is localhost/(local)/127.0.0.1
-jdbc:sqlserver://localhost;database=tse-internal-spam-testing;user=username@some-database-name;password=password_10;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
-jdbc:sqlserver://(local);database=tse-internal-spam-testing;user=username@some-database-name;password=password_11;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
-jdbc:sqlserver://127.0.0.1;database=tse-internal-spam-testing;user=username@some-database-name;password=password_12;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+jdbc:sqlserver://localhost;database=database11;user=username11@some-database-name;password=password11;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+jdbc:sqlserver://(local);database=database12;user=username12@some-database-name;password=password12;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+jdbc:sqlserver://127.0.0.1;database=database13;user=username13@some-database-name;password=password13;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+
+
+[global::System.Configuration.DefaultSettingValueAttribute("Data Source=servername14;Initial Catalog=database14;User ID=username14;Password=password14")]
+        public string ELSConnectionString {
