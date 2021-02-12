e8e5124d08d44b38a08409ed3bc70767cf455ee0
Eddy Nakamura <ednakamu@microsoft.com>
2021-02-12 09:21:59 -0300

    Basic header authorization

---
+++ Src/Plugins/Security/SEC101_001.HttpAuthorizationRequestHeaderValidator.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class HttpAuthorizationRequestHeaderValidator : ValidatorBase
+    {
+        internal static HttpAuthorizationRequestHeaderValidator Instance;
+
+        static HttpAuthorizationRequestHeaderValidator()
+        {
+            Instance = new HttpAuthorizationRequestHeaderValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return ValidatorBase.IsValidStatic(Instance,
+                                               ref matchedPattern,
+                                               ref groups,
+                                               ref failureLevel,
+                                               ref fingerprint,
+                                               ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetValue("host", out string host) ||
+                !groups.TryGetValue("key", out string key))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint
+            {
+                Host = host,
+                Key = key,
+                Resource = groups.ContainsKey("resource") ? groups["resource"] : string.Empty,
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+    }
+}
