a521770bf29bc2ca5b1032d5bb374141813eb6ee
Eddy Nakamura <ednakamu@microsoft.com>
2021-02-16 17:28:37 -0300

    Improvement in HttpAuthorizationRequestHeader validation

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_001.HttpAuthorizationRequestHeader.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_001.HttpAuthorizationRequestHeader.ps1
@@ -7,6 +7,12 @@ var client = new RestClient("https://example.com?some=parameters&that=should&app
 var request = new RestRequest(Method.GET);
 request.AddHeader("Authorization", "Basic SomeAuthorizationKey0123456");
 
+# This should not get caught, since it will drop the last slash
+# and would be equal to the first example.
+var client = new RestClient("https://example.com/")
+var request = new RestRequest(Method.GET);
+request.AddHeader("Authorization", "Basic SomeAuthorizationKey0123456");
+
 # cURL
 curl --location --request GET 'https://example.com' \
 --header 'Authorization: Basic SomeAuthorizationKey0123456=' \
