cb0f604a2059c92e53bee16cb0f86c4c1de42a8d
Eddy Nakamura <ednakamu@microsoft.com>
2021-02-17 10:50:39 -0300

    Enable Square fingerprint

---
+++ Src/Plugins/Security/SEC101_010.SquarePatValidator.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class SquarePatValidator : ValidatorBase
+    {
+        internal static SquarePatValidator Instance;
+
+        static SquarePatValidator()
+        {
+            Instance = new SquarePatValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return ValidatorBase.IsValidStatic(Instance,
+                                               ref matchedPattern,
+                                               ref groups,
+                                               ref failureLevel,
+                                               ref fingerprint,
+                                               ref message);
+        }
+
+        public static string IsValidDynamic(ref string fingerprint, ref string message)
+        {
+            return ValidatorBase.IsValidDynamic(Instance,
+                                                ref fingerprint,
+                                                ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetValue("key", out string key))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint
+            {
+                PersonalAccessToken = key,
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+
+        protected override string IsValidDynamicHelper(ref string fingerprintText,
+                                                       ref string message)
+        {
+            var fingerprint = new Fingerprint(fingerprintText);
+            string pat = fingerprint.PersonalAccessToken;
+
+            try
+            {
+                message = $"The detected secret is a {pat} key.";
+
+                using HttpClient client = CreateHttpClient();
+
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", pat);
+
+                using HttpResponseMessage response = client
+                    .GetAsync($"https://connect.squareup.com/v2/catalog/list")
+                    .GetAwaiter()
+                    .GetResult();
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    {
+                        return nameof(ValidationState.Authorized);
+                    }
+
+                    case HttpStatusCode.Unauthorized:
+                    {
+                        return nameof(ValidationState.Unauthorized);
+                    }
+
+                    default:
+                    {
+                        message += $" An unexpected response code was returned attempting to " +
+                                  $"validate: '{response.StatusCode}'";
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return ReturnUnhandledException(ref message, e);
+            }
+
+            return nameof(ValidationState.Unknown);
+        }
+    }
+}
---
+++ Src/Plugins/Security/SEC101_011.SquareCredentialsValidator.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class SquareCredentialsValidator : ValidatorBase
+    {
+        internal static SquareCredentialsValidator Instance;
+
+        static SquareCredentialsValidator()
+        {
+            Instance = new SquareCredentialsValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return ValidatorBase.IsValidStatic(Instance,
+                                               ref matchedPattern,
+                                               ref groups,
+                                               ref failureLevel,
+                                               ref fingerprint,
+                                               ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetValue("id", out string id) ||
+                !groups.TryGetValue("key", out string key))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint
+            {
+                Id = id,
+                Key = key,
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+    }
+}
