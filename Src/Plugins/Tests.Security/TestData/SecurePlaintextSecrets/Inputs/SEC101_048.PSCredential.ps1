1e77e88a63f65df036407bbad137fb95bb1e3139
James Winkler <v-jwinkler@microsoft.com>
2021-03-01 14:25:11 -0800

    Add one more sub rule

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_048.PSCredential.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_048.PSCredential.ps1
@@ -4,9 +4,14 @@ $domainCredential = New-Object System.Management.Automation.PSCredential ("REDMO
 # Implemented in PSCredentialConstructor
  PSCredential Credential = new PSCredential ( "username2" , "doodle_BLOB2");
 
-
+ # Implemented in PSCredentialObjectInitializer
  something = new PSCredential()
  {
 	UserName = "username3",
 	Password = "doodle_BLOB3",
  }
+
+ # Example from https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-credential?view=powershell-7.1
+ # Implemented in PSCredentialPowershell
+ $PWord = ConvertTo-SecureString -String "doodle_BLOB6" -AsPlainText -Force
+ $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
