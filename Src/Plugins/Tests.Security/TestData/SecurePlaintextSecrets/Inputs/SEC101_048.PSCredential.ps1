41e920b49fd739606ea4fbdf6793c66e2a990735
James Winkler <v-jwinkler@microsoft.com>
2021-03-01 13:35:13 -0800

    Add PSCredentialValidator

---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_048.PSCredential.ps1
@@ -0,0 +1,12 @@
+# Implemented in PSCredentialConstructor
+$domainCredential = New-Object System.Management.Automation.PSCredential ("REDMOND\v-jwinkler", "doodle_BLOB1");
+
+# Implemented in PSCredentialConstructor
+ PSCredential Credential = new PSCredential ( "username2" , "doodle_BLOB2");
+
+
+ something = new PSCredential()
+ {
+	UserName = "username3",
+	Password = "doodle_BLOB3",
+ }
