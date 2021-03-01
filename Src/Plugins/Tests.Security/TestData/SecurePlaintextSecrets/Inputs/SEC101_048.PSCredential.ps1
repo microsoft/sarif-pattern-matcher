8e3e67e1da803e3c739d999268cacbb4d54eb90c
James Winkler <v-jwinkler@microsoft.com>
2021-03-01 14:53:12 -0800

    Use dummy username

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_048.PSCredential.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_048.PSCredential.ps1
@@ -1,5 +1,5 @@
 # Implemented in PSCredentialConstructor
-$domainCredential = New-Object System.Management.Automation.PSCredential ("REDMOND\v-jwinkler", "doodle_BLOB1");
+$domainCredential = New-Object System.Management.Automation.PSCredential ("REDMOND\username1", "doodle_BLOB1");
 
 # Implemented in PSCredentialConstructor
  PSCredential Credential = new PSCredential ( "username2" , "doodle_BLOB2");
