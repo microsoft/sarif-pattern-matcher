10b7df2b6b6bf861ca623b422879f44d4384d3fc
James Winkler <v-jwinkler@microsoft.com>
2021-03-01 15:10:56 -0800

    Add new PSCredentialsValidator

---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_048.PSCredential.ps1
@@ -0,0 +1,17 @@
+# Implemented in PSCredentialConstructor
+$domainCredential = New-Object System.Management.Automation.PSCredential ("REDMOND\username1", "doodle_BLOB1");
+
+# Implemented in PSCredentialConstructor
+ PSCredential Credential = new PSCredential ( "username2" , "doodle_BLOB2");
+
+ # Implemented in PSCredentialObjectInitializer
+ something = new PSCredential()
+ {
+	UserName = "username3",
+	Password = "doodle_BLOB3",
+ }
+
+ # Example from https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-credential?view=powershell-7.1
+ # Implemented in PSCredentialPowershell
+ $PWord = ConvertTo-SecureString -String "doodle_BLOB6" -AsPlainText -Force
+ $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
