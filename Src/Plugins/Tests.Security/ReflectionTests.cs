ec3009562ba5197114824a0ae5ba40600b2b881c
James Winkler <v-jwinkler@microsoft.com>
2021-03-02 15:09:15 -0800

    Add new test, non-functional

---
+++ Src/Plugins/Tests.Security/ReflectionTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class ReflectionTests
+    {
+        [Fact]
+        public void All101Rules_ShouldHaveValidators()
+        {
+            try
+            {
+                using (StreamReader file = File.OpenText(@"SEC101.SecurePlaintextSecrets.json"))
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject o2 = (JObject)JToken.ReadFrom(reader);
+                }
+            }
+            catch (IOException ioe)
+            {
+                Assert.True(false, "Failed to read the rules file.");
+            }
+            
+        }
+    }
+}
