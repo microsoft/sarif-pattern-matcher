cd3d1a7654ba42865a3e0a12cc32ac70f41eeff5
James Winkler <v-jwinkler@microsoft.com>
2021-03-02 16:20:39 -0800

    Add unit test for all rules having validators

--- Src/Plugins/Tests.Security/ReflectionTests.cs
+++ Src/Plugins/Tests.Security/ReflectionTests.cs
@@ -4,7 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Linq;
+using System.Reflection;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -24,13 +25,39 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
                 using (JsonTextReader reader = new JsonTextReader(file))
                 {
                     JObject o2 = (JObject)JToken.ReadFrom(reader);
+
+                    IEnumerable<string> rules = o2["Definitions"][0]["MatchExpressions"].Select(x => x["Name"].ToString().Split('/')[1]).Distinct();
+
+                    Assembly assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => x.GetName().Name == "Security").Single();
+                    HashSet<string> validators = assemblies.GetTypes().Where(x => x.Name.EndsWith("Validator")).Select(x => x.Name).ToHashSet();
+
+                    List<string> rulesWithoutValidators = new List<string>();
+
+                    foreach(string rule in rules)
+                    {
+                        if (!validators.TryGetValue(rule + "Validator", out string _))
+                        {
+                            rulesWithoutValidators.Add(rule);
+                        }
+                    }
+
+                    // Assert.Empty doesn't allow custom messages, so use Assert.True
+                    Assert.True(rulesWithoutValidators.Count == 0, "Unable to find validators for these rules: " + string.Join(',', rulesWithoutValidators));
                 }
             }
-            catch (IOException ioe)
+            catch (IOException)
             {
                 Assert.True(false, "Failed to read the rules file.");
             }
-            
+            catch (NullReferenceException)
+            {
+                Assert.True(false, "Unexpected JSON structure in rules file");
+            }
+            catch (ArgumentNullException)
+            {
+                Assert.True(false, "Unable to find Security assembly, has the name changed?");
+            }
+
         }
     }
 }
