6f289822b8ef51060883940e4f614e939a2f34f5
James Winkler <v-jwinkler@microsoft.com>
2021-03-05 10:55:17 -0800

    New UserPasswordCredential Validator

---
+++ Src/Plugins/Security/SEC101_053.UserPasswordCredentialValidator.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class UserPasswordCredentialValidator : ValidatorBase
+    {
+        internal static UserPasswordCredentialValidator Instance;
+
+        static UserPasswordCredentialValidator()
+        {
+            Instance = new UserPasswordCredentialValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return IsValidStatic(Instance,
+                                 ref matchedPattern,
+                                 ref groups,
+                                 ref failureLevel,
+                                 ref fingerprint,
+                                 ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetNonEmptyValue("password", out string password) ||
+                !groups.TryGetNonEmptyValue("account", out string account))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint()
+            {
+                Account = account,
+                Password = password,
+            }.ToString();
+
+            return nameof(ValidationState.AuthorizedWarning);
+        }
+    }
+}
---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_053.UserPasswordCredential.ps1
@@ -0,0 +1,5 @@
+new UserPasswordCredential("username1", "Doodle_BLOB1"
+
+new UserPasswordCredential("username2@subdomain.onmicrosoft.com", "Doodle2!"
+
+new UserPasswordCredential("usernam3@submdomain.onmicrosoft.com", "D$$dle3"
