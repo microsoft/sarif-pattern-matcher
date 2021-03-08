9270a7d4dfa309cfea085aafb12ec8c4eb88c0fe
Eddy Nakamura <ednakamu@microsoft.com>
2021-03-08 16:33:09 -0300

    Enable NPM Validator

---
+++ Src/Plugins/Security/SEC101_017.NpmApiKeyValidator.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class NpmApiKeyValidator : ValidatorBase
+    {
+        internal static NpmApiKeyValidator Instance;
+
+        static NpmApiKeyValidator()
+        {
+            Instance = new NpmApiKeyValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return ValidatorBase.IsValidStatic(Instance,
+                                               ref matchedPattern,
+                                               ref groups,
+                                               ref failureLevel,
+                                               ref fingerprint,
+                                               ref message);
+        }
+
+        public static string IsValidDynamic(ref string fingerprint, ref string message)
+        {
+            return ValidatorBase.IsValidDynamic(Instance,
+                                                ref fingerprint,
+                                                ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetNonEmptyValue("key", out string key))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint
+            {
+                Key = key,
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+
+        protected override string IsValidDynamicHelper(ref string fingerprintText,
+                                                       ref string message)
+        {
+            var fingerprint = new Fingerprint(fingerprintText);
+
+            string key = fingerprint.Key;
+
+            try
+            {
+                using HttpClient client = CreateHttpClient();
+
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", key);
+
+                using HttpResponseMessage response = client
+                    .GetAsync($"https://registry.npmjs.org/-/whoami")
+                    .GetAwaiter()
+                    .GetResult();
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    {
+                        return nameof(ValidationState.AuthorizedError);
+                    }
+
+                    case HttpStatusCode.Unauthorized:
+                    {
+                        return nameof(ValidationState.Unauthorized);
+                    }
+
+                    default:
+                    {
+                        message += $" An unexpected response code was returned attempting to " +
+                                  $"validate: '{response.StatusCode}'";
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return ReturnUnhandledException(ref message, e);
+            }
+
+            return nameof(ValidationState.Unknown);
+        }
+    }
+}
