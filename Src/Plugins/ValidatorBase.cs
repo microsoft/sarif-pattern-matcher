84c8d018f58e995a5867f5d7f0ceb26a6ba4856a
Eddy Nakamura <ednakamu@microsoft.com>
2021-03-09 11:39:35 -0300

    fixing formatting

--- Src/Plugins/ValidatorBase.cs
+++ Src/Plugins/ValidatorBase.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
+
 using Microsoft.RE2.Managed;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins
