b2181757ad90b752dfbbeea9342d110000f81647
James Winkler <v-jwinkler@microsoft.com>
2021-03-11 16:01:24 -0800

    Add dynamic validation

--- Src/Plugins/Security/SEC101_055.UserPasswordCredentialValidator.cs
+++ Src/Plugins/Security/SEC101_055.UserPasswordCredentialValidator.cs
@@ -1,15 +1,25 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Security;
+
+using GoogleApi.Entities.Maps.Elevation.Response;
 
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities;
+using Microsoft.Identity.Client;
+
+using Org.BouncyCastle.Asn1.X509.Qualified;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
 {
     public class UserPasswordCredentialValidator : ValidatorBase
     {
+        private const string UnknownUserType = "unknown_user_type";
+        private const string ParsingWSTrustResponseFailed = "parsing_wstrust_response_failed";
+        private const string UnauthorizedClient = "unauthorized_client";
+
         internal static UserPasswordCredentialValidator Instance;
 
         static UserPasswordCredentialValidator()
@@ -31,6 +41,13 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
                                  ref message);
         }
 
+        public static string IsValidDynamic(ref string fingerprint, ref string message)
+        {
+            return IsValidDynamic(Instance,
+                                   ref fingerprint,
+                                   ref message);
+        }
+
         protected override string IsValidStaticHelper(ref string matchedPattern,
                                                       ref Dictionary<string, string> groups,
                                                       ref string failureLevel,
@@ -50,6 +67,71 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
             }.ToString();
 
             return nameof(ValidationState.AuthorizedWarning);
+        }
+
+        protected override string IsValidDynamicHelper(ref string fingerprintText,
+                                                       ref string message)
+        {
+            var fingerprint = new Fingerprint(fingerprintText);
+            string account = fingerprint.Account;
+            string password = fingerprint.Password;
+
+            string authority = "https://login.microsoftonline.com/contoso.com";
+            string[] scopes = new string[] { "user.read" };
+            var securePassword = new SecureString();
+            string clientId = "test";
+
+            // MSAL requires that we enter the password one character at a time
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+
+            IPublicClientApplication app;
+            app = PublicClientApplicationBuilder.Create(clientId)
+                  .WithAuthority(authority)
+                  .Build();
+
+            AuthenticationResult result = null;
+            try
+            {
+                result = app.AcquireTokenByUsernamePassword(scopes,
+                                                            account,
+                                                            securePassword).ExecuteAsync().Result;
+            }
+            catch (AggregateException ae)
+            {
+                var clientException = ae.InnerExceptions[0] as MsalClientException;
+                if (clientException != null)
+                {
+                    switch (clientException.ErrorCode)
+                    {
+                        case UnknownUserType:
+                            // Username probably missing @microsoft.com or something similar
+                        case ParsingWSTrustResponseFailed:
+                            // One or both of username and password is wrong
+                            return ReturnUnauthorizedAccess(ref message, asset: account);
+                        default:
+                            break;
+                    }
+                }
+
+                var serviceException = ae.InnerExceptions[0] as MsalServiceException;
+                if (serviceException != null)
+                {
+                    if (serviceException.ErrorCode == UnauthorizedClient)
+                    {
+                        // Username right, password right, dummy client id
+                        return ReturnAuthorizedAccess(ref message, asset: account);
+                    }
+                }
+
+                return ReturnUnhandledException(ref message, ae);
+            }
+
+            // Should be unreachable if all goes as expected
+            message = $"Unexpected MSAL behavior, the access token was {(string.IsNullOrWhiteSpace(result?.AccessToken) ? string.Empty : "not ")}empty";
+            return nameof(ValidationState.Unknown);
         }
     }
 }
---
+++ Src/Plugins/Tests.Security/Validators/SEC101_055.UserPasswordCredentialValidatorTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validators
+{
+    public class UserPasswordCredentialValidatorTests
+    {
+        private const string expectedValidationState = nameof(ValidationState.Unauthorized);
+        private const string username = "username1@microsoft.com";
+        private const string password = "Doodle_BLOB1";
+
+        [Fact]
+        public void InvalidCredentials_ShouldBeUnauthorized()
+        {
+            UserPasswordCredentialValidator validator = new UserPasswordCredentialValidator();
+            string fingerprintText = $"[acct={username}][pwd={password}]";
+            string message = null;
+            string actualValidationState = UserPasswordCredentialValidator.IsValidDynamic(ref fingerprintText, ref message);
+            Assert.Equal(expectedValidationState, actualValidationState);
+        }
+    }
+}
