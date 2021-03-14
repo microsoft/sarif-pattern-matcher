a80aafa6118a2bdbc06253f191388ce070b24fce
Eddy Nakamura <eddynaka@gmail.com>
2021-03-14 17:20:57 -0300

    Improving message handling (#300)
    * Improving message handling
    
    * Update SEC101_003.GoogleApiKeyValidator.cs

--- Src/Plugins/Security/SEC101_003.GoogleApiKeyValidator.cs
+++ Src/Plugins/Security/SEC101_003.GoogleApiKeyValidator.cs
@@ -58,12 +58,15 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
         protected override string IsValidDynamicHelper(ref string fingerprintText,
                                                        ref string message)
         {
+            const string Invalid = "RequestDenied: The provided API key is invalid";
+            const string Expired = "RequestDenied: The provided API key is expired";
+            const string RevokedKey = "RequestDenied: Google has disabled the use of APIs from this API project";
+            const string ProjectNotAuthorized = "RequestDenied: This API project is not authorized to use this API";
+            const string KeyNotAuthorized = "RequestDenied: This API key is not authorized to use this service or API";
+            const string RefererRestrictions = "RequestDenied: API keys with referer restrictions cannot be used with this API";
+            const string IPNotAuthorized = "RequestDenied: This IP, site or mobile application is not authorized to use this API key";
             const string EnableBilling = "RequestDenied: You must enable Billing on the Google Cloud Project at https://console.cloud.google.com/project/_/billing/enable Learn more at https://developers.google.com/maps/gmp-get-started";
-            const string Deleted = "RequestDenied: This API project was not found. This API project may have been deleted or may not be authorized to use this API. You may need to enable the API under APIs in the console.";
-            const string KeyNotAuthorized = "RequestDenied: This API key is not authorized to use this service or API.";
-            const string ProjectNotAuthorized = "RequestDenied: This API project is not authorized to use this API.";
-            const string Invalid = "RequestDenied: The provided API key is invalid.";
-            const string Expired = "RequestDenied: The provided API key is expired.";
+            const string Deleted = "RequestDenied: This API project was not found. This API project may have been deleted or may not be authorized to use this API. You may need to enable the API under APIs in the console";
 
             var fingerprint = new Fingerprint(fingerprintText);
 
@@ -82,35 +85,39 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
             }
             catch (Exception e)
             {
-                if (e is GoogleApiException googleException)
+                if (e is GoogleApiException)
                 {
-                    switch (e.Message)
+                    if (e.Message.StartsWith(RevokedKey) ||
+                        e.Message.StartsWith(IPNotAuthorized) ||
+                        e.Message.StartsWith(RefererRestrictions))
                     {
-                        case EnableBilling:
-                        {
-                            // The API is enabled but billing has not been configured.
-                            return nameof(ValidationState.AuthorizedError);
-                        }
+                        return nameof(ValidationState.Unauthorized);
+                    }
 
-                        case ProjectNotAuthorized:
-                        case KeyNotAuthorized:
-                        {
-                            // What this condition means is that the API key is recognized.
-                            // It is not authorized for the Directions API, but this isn't
-                            // what we're verifying here.
-                            return nameof(ValidationState.AuthorizedError);
-                        }
+                    if (e.Message.StartsWith(Deleted) ||
+                        e.Message.StartsWith(Expired))
+                    {
+                        return nameof(ValidationState.Expired);
+                    }
 
-                        case Deleted:
-                        case Expired:
-                        {
-                            return nameof(ValidationState.Expired);
-                        }
+                    if (e.Message.StartsWith(EnableBilling))
+                    {
+                        // The API is enabled but billing has not been configured.
+                        return nameof(ValidationState.AuthorizedError);
+                    }
 
-                        case Invalid:
-                        {
-                            return nameof(ValidationState.NoMatch);
-                        }
+                    if (e.Message.StartsWith(KeyNotAuthorized) ||
+                        e.Message.StartsWith(ProjectNotAuthorized))
+                    {
+                        // What this condition means is that the API key is recognized.
+                        // It is not authorized for the Directions API, but this isn't
+                        // what we're verifying here.
+                        return nameof(ValidationState.AuthorizedError);
+                    }
+
+                    if (e.Message.StartsWith(Invalid))
+                    {
+                        return nameof(ValidationState.NoMatch);
                     }
                 }
 
