303ecb8cc83b19369fb652d80ca9524559950ba8
Eddy Nakamura <ednakamu@microsoft.com>
2021-03-19 09:35:25 -0300

    Adding dropbox validators

---
+++ Src/Plugins/Security/SEC101_020.DropboxAccessTokenValidator.cs
@@ -0,0 +1,133 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class DropboxAccessTokenValidator : ValidatorBase
+    {
+        internal static DropboxAccessTokenValidator Instance;
+
+        static DropboxAccessTokenValidator()
+        {
+            Instance = new DropboxAccessTokenValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return IsValidStatic(Instance,
+                                 ref matchedPattern,
+                                 ref groups,
+                                 ref failureLevel,
+                                 ref fingerprint,
+                                 ref message);
+        }
+
+        public static string IsValidDynamic(ref string fingerprint, ref string message, ref Dictionary<string, string> options)
+        {
+            return IsValidDynamic(Instance,
+                                  ref fingerprint,
+                                  ref message,
+                                  ref options);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetNonEmptyValue("refine", out string key))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            if (!ContainsDigitAndChar(key))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint()
+            {
+                Key = key,
+                Platform = nameof(AssetPlatform.Dropbox),
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+
+        protected override string IsValidDynamicHelper(ref string fingerprintText, ref string message, ref Dictionary<string, string> options)
+        {
+            var fingerprint = new Fingerprint(fingerprintText);
+            string key = fingerprint.Key;
+            using HttpClient httpClient = CreateHttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+
+            try
+            {
+                using HttpResponseMessage response = httpClient
+                    .PostAsync("https://api.dropboxapi.com/2/file_requests/count", null)
+                    .GetAwaiter()
+                    .GetResult();
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    {
+                        return nameof(ValidationState.AuthorizedError);
+                    }
+
+                    case HttpStatusCode.BadRequest:
+                    {
+                        string body = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                        // App deleted.
+                        if (body.Contains("This app is currently disabled"))
+                        {
+                            return nameof(ValidationState.Expired);
+                        }
+
+                        // Request was successfull but AccessToken does not have access.
+                        if (body.Contains("file_requests.read"))
+                        {
+                            return nameof(ValidationState.AuthorizedError);
+                        }
+
+                        // We don't recognize this message.
+                        message = CreateUnexpectedResponseCodeMessage(response.StatusCode);
+                        break;
+                    }
+
+                    case HttpStatusCode.Unauthorized:
+                    {
+                        return nameof(ValidationState.Unauthorized);
+                    }
+
+                    default:
+                    {
+                        message = CreateUnexpectedResponseCodeMessage(response.StatusCode);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return ReturnUnhandledException(ref message, e);
+            }
+
+            return nameof(ValidationState.Unknown);
+        }
+    }
+}
