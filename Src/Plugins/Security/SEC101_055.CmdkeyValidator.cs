1a740d9db464ed70c91c84f7ec27414100edf689
James Winkler <v-jwinkler@microsoft.com>
2021-03-19 15:36:07 -0700

    Add new Cmdkey validator

---
+++ Src/Plugins/Security/SEC101_055.CmdkeyValidator.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+using Microsoft.RE2.Managed;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class CmdkeyValidator : ValidatorBase
+    {
+        internal static CmdkeyValidator Instance;
+        internal static IRegex RegexEngine;
+
+        private const string UserRegex = @"\/user:\s*([^\[""'\s`%]{2,})";
+
+        static CmdkeyValidator()
+        {
+            Instance = new CmdkeyValidator();
+            RegexEngine = RE2Regex.Instance;
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return IsValidStatic(Instance,
+                                 ref matchedPattern,
+                                 ref groups,
+                                 ref failureLevel,
+                                 ref fingerprint,
+                                 ref message);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetNonEmptyValue("password", out string password))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            if (IsInvalidPassword(password))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            string account = ParseExpression(RegexEngine, matchedPattern, UserRegex);
+            account = account?.Replace("/user:", string.Empty).Trim();
+
+            fingerprintText = new Fingerprint()
+            {
+                Account = account,
+                Password = password,
+            }.ToString();
+
+            // Manual use of the regex against CodeAsData produces highly confident results,
+            // so return AuthorizedWarning rather than Unknown.
+            return nameof(ValidationState.AuthorizedWarning);
+        }
+
+        private static bool IsInvalidPassword(string potentialPassword)
+        {
+            if (string.IsNullOrWhiteSpace(potentialPassword))
+            {
+                return true;
+            }
+
+            if (potentialPassword.Length < 2)
+            {
+                return true;
+            }
+
+            if (potentialPassword.First() == '$')
+            {
+                // The command looks like
+                // cmdkey /pass: $passwordVariable
+                // This is not a match
+                return true;
+            }
+
+            // The following may be valid characters in the middle of a password, but
+            // seeing them on both ends specifically is HIGHLY indicative that a variable
+            // was passed into the command.
+            if (potentialPassword.First() == '<' && potentialPassword.Last() == '>')
+            {
+                return true;
+            }
+
+            if (potentialPassword.First() == '{' && potentialPassword.Last() == '}')
+            {
+                return true;
+            }
+
+            if (potentialPassword.First() == '(' && potentialPassword.Last() == ')')
+            {
+                return true;
+            }
+
+            if (potentialPassword.First() == '[' && potentialPassword.Last() == ']')
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_055.Cmdkey.ps1
@@ -0,0 +1,12 @@
+\n[string]$result = cmdkey /add:testpowershell /user:testuser1 /pass:1111\n\nWrite-Host $result\n\nWrite-Host \"Variablevalue is : $ENV:TestVariable\""
+
+pression -Command \"cmdkey /add:name.file.core.windows.net /user:Azure\\testuser2 /pass:2222222/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/2B2B2B2B2B2B2B==\"\n# do stuff\nNew-PSDrive -Name Z -PSProvider FileSystem -Root \"\\\\name.file.core.windows.net\\release-pipeline\"\n\nWrite-Host \"Coping Files\"\n\dir -Force \"Z:\\${env:
+
+"inputs":{"script":"cmdkey /generic:name-test/email@live.com/signInPassword /user:testuser3@live.com /pass:@3C3C3C3C3C3C\n\ntsc portaltests.ts --module commonjs --lib stuff --moduleResolution classic & node_modules\"
+
+# Regex should catch this but Validator will clean it up
+psi.FileName = "cmdkey.exe";                 psi.Arguments = string.Format(@"/add:{0} /user:{1}\{testuser4} /pass:{4444}", server, domain, userName, password);
+
+cmdkey /add:<ServerName> /user:<testuser5> /pass:<5555>
+
+$cmdkeyResults = & cmdkey /generic:$ssPath /user:testuser6 /pass:$6666666
