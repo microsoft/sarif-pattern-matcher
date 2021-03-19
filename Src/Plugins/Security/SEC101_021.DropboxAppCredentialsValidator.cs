9ef2fca2cb132d4f9df995298ad86d030a25af87
Eddy Nakamura <eddynaka@gmail.com>
2021-03-19 20:16:33 -0300

    Adding dropbox validators (#315)
    * Adding dropbox validators
    
    * Addressing PR feedback

---
+++ Src/Plugins/Security/SEC101_021.DropboxAppCredentialsValidator.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class DropboxAppCredentialsValidator : ValidatorBase
+    {
+        internal static DropboxAppCredentialsValidator Instance;
+
+        static DropboxAppCredentialsValidator()
+        {
+            Instance = new DropboxAppCredentialsValidator();
+        }
+
+        public static string IsValidStatic(ref string matchedPattern,
+                                           ref Dictionary<string, string> groups,
+                                           ref string failureLevel,
+                                           ref string fingerprint,
+                                           ref string message)
+        {
+            return IsValidStatic(Instance,
+                                 ref matchedPattern,
+                                 ref groups,
+                                 ref failureLevel,
+                                 ref fingerprint,
+                                 ref message);
+        }
+
+        public static string IsValidDynamic(ref string fingerprint, ref string message, ref Dictionary<string, string> options)
+        {
+            return IsValidDynamic(Instance,
+                                  ref fingerprint,
+                                  ref message,
+                                  ref options);
+        }
+
+        protected override string IsValidStaticHelper(ref string matchedPattern,
+                                                      ref Dictionary<string, string> groups,
+                                                      ref string failureLevel,
+                                                      ref string fingerprintText,
+                                                      ref string message)
+        {
+            if (!groups.TryGetNonEmptyValue("key", out string key) ||
+                !groups.TryGetNonEmptyValue("secret", out string secret))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            if (!ContainsDigitAndChar(key) ||
+                !ContainsDigitAndChar(secret))
+            {
+                return nameof(ValidationState.NoMatch);
+            }
+
+            fingerprintText = new Fingerprint()
+            {
+                Id = key,
+                Key = secret,
+                Platform = nameof(AssetPlatform.Dropbox),
+            }.ToString();
+
+            return nameof(ValidationState.Unknown);
+        }
+
+        protected override string IsValidDynamicHelper(ref string fingerprintText, ref string message, ref Dictionary<string, string> options)
+        {
+            var fingerprint = new Fingerprint(fingerprintText);
+            string id = fingerprint.Id;
+            string key = fingerprint.Key;
+            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", id, key)));
+            using HttpClient httpClient = CreateHttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+            httpClient.DefaultRequestHeaders.Add("Dropbox-API-Arg", @"{""resource"": {"".tag"": ""path"",""path"": ""/a.docx""},""format"": ""jpeg"",""size"": ""w64h64"",""mode"": ""strict""}");
+
+            try
+            {
+                using HttpResponseMessage response = httpClient
+                    .PostAsync("https://content.dropboxapi.com/2/files/get_thumbnail_v2", null)
+                    .GetAwaiter()
+                    .GetResult();
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.InternalServerError:
+                    {
+                        // The request is correct, but the header 'Dropbox-API-Arg' is wrong.
+                        return nameof(ValidationState.AuthorizedError);
+                    }
+
+                    case HttpStatusCode.BadRequest:
+                    {
+                        string body = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                        // App deleted.
+                        if (body.Contains("App has been disabled"))
+                        {
+                            return nameof(ValidationState.Expired);
+                        }
+
+                        if (body.Contains("invalid_client"))
+                        {
+                            return nameof(ValidationState.Unauthorized);
+                        }
+
+                        // We don't recognize this message.
+                        message = CreateUnexpectedResponseCodeMessage(response.StatusCode);
+                        break;
+                    }
+
+                    default:
+                    {
+                        message = CreateUnexpectedResponseCodeMessage(response.StatusCode);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return ReturnUnhandledException(ref message, e);
+            }
+
+            return nameof(ValidationState.Unknown);
+        }
+    }
+}
---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_021.DropboxAppCredentials.ps1
@@ -0,0 +1,2 @@
+dropboxKey: g09uoc368gvgt7h
+dropboxSecret: sf05okpc1xd6b18
