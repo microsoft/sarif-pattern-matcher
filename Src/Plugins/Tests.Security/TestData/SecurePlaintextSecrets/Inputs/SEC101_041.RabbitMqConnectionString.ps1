b315f6cd1cfe826e4d6f78b459c6a99a8ecb33d3
Eddy Nakamura <ednakamu@microsoft.com>
2021-03-25 09:29:30 -0300

    Improving RabbitMQ regex/tests

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_041.RabbitMqConnectionString.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_041.RabbitMqConnectionString.ps1
@@ -1,5 +1,15 @@
+# Simple string
 amqp://user:password@host/database1
 
-amqp://user:password@host/database2"
+# Simple string as value
+"amqp://user:password@host/database2"
 
->amqps://user:password@host/database3<
+# Simple string as XML
+<xml>amqps://user:password@host/database3</xml>
+
+# Simple string with port
+amqp://user:password@host:1234/database4
+
+# This is invalid and should not be captured
+amqp://user:password@host:1234 /database5
+amqp://user:password@host:1234
