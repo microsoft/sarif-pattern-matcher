769b4cd511e71fd89f3227c2adbd47b8ff1fc856
Eddy Nakamura <ednakamu@microsoft.com>
2021-04-01 13:59:17 -0300

    Exporting rules metadata

---
+++ Src/Sarif.PatternMatcher.Cli/ExportRulesMetatadaCommand.cs
@@ -0,0 +1,148 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Microsoft.CodeAnalysis.Sarif.Driver;
+
+using Newtonsoft.Json;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli
+{
+    internal class ExportRulesMetatadaCommand : CommandBase
+    {
+        public int Run(ExportRulesMetatadaOptions options)
+        {
+            try
+            {
+                if (!ValidateOptions(options))
+                {
+                    return FAILURE;
+                }
+
+                ImportSearchDefinitions(options, out List<SearchDefinitions> searchDefinitionsList, out ValidatorsCache validators);
+
+                File.WriteAllText(options.OutputFilePath, ProcessSearchDefinitions(searchDefinitionsList, validators));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return FAILURE;
+            }
+
+            return SUCCESS;
+        }
+
+        public bool ValidateOptions(ExportRulesMetatadaOptions options)
+        {
+            if (options.SearchDefinitionsPaths?.Any() != true)
+            {
+                return false;
+            }
+
+            foreach (string searchDefinition in options.SearchDefinitionsPaths)
+            {
+                if (!FileSystem.FileExists(searchDefinition))
+                {
+                    return false;
+                }
+            }
+
+            return !string.IsNullOrEmpty(options.OutputFilePath);
+        }
+
+        private void ImportSearchDefinitions(ExportRulesMetatadaOptions options, out List<SearchDefinitions> definitions, out ValidatorsCache validators)
+        {
+            definitions = new List<SearchDefinitions>();
+            validators = new ValidatorsCache();
+
+            foreach (string searchDefinitionsPath in options.SearchDefinitionsPaths)
+            {
+                string searchDefinitionsText =
+                    FileSystem.FileReadAllText(searchDefinitionsPath);
+
+                SearchDefinitions currentDefinitions =
+                    JsonConvert.DeserializeObject<SearchDefinitions>(searchDefinitionsText);
+
+                // This would skip files that does not look like rules.
+                if (currentDefinitions == null || currentDefinitions.Definitions == null)
+                {
+                    continue;
+                }
+
+                string definitionsDirectory = Path.GetDirectoryName(searchDefinitionsPath);
+
+                string validatorPath;
+                if (!string.IsNullOrEmpty(currentDefinitions.ValidatorsAssemblyName))
+                {
+                    validatorPath = Path.Combine(definitionsDirectory, currentDefinitions.ValidatorsAssemblyName);
+                    validators.ValidatorPaths.Add(validatorPath);
+                }
+                else
+                {
+                    // If no explicit name of a validator binary was provided,
+                    // we look for one that lives alongside the definitions file.
+                    validatorPath = Path.GetFileNameWithoutExtension(searchDefinitionsPath) + ".dll";
+                    validatorPath = Path.Combine(definitionsDirectory, validatorPath);
+
+                    if (File.Exists(validatorPath))
+                    {
+                        validators.ValidatorPaths.Add(validatorPath);
+                    }
+                }
+
+                definitions.Add(currentDefinitions);
+            }
+        }
+
+        private string ProcessSearchDefinitions(List<SearchDefinitions> searchDefinitionsList, ValidatorsCache validators)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("# Rules");
+            builder.AppendLine();
+
+            foreach (SearchDefinitions searchDefinition in searchDefinitionsList.OrderBy(d => d.ValidatorsAssemblyName))
+            {
+                var hash = new HashSet<string>();
+
+                builder.AppendLine($"## {searchDefinition.ValidatorsAssemblyName} ({searchDefinition.Definitions.Count})");
+                builder.AppendLine();
+
+                foreach (SearchDefinition definition in searchDefinition.Definitions.OrderBy(d => d.Id))
+                {
+                    builder.AppendLine($"### {definition.Id}.{definition.Name} ({definition.MatchExpressions.GroupBy(g => g.Id).Count()})");
+                    builder.AppendLine("ID | Name | Validation");
+                    builder.AppendLine("---| --- | ---");
+
+                    foreach (MatchExpression match in definition.MatchExpressions.OrderBy(m => m.Id))
+                    {
+                        string key = match.Id + match.Name;
+                        if (!hash.Contains(key))
+                        {
+                            ValidationMethods validationPair = null;
+
+                            if (!string.IsNullOrEmpty(match.Name))
+                            {
+                                validationPair = ValidatorsCache.GetValidationMethods(match.Name, validators.RuleNameToValidationMethods);
+                            }
+
+                            builder.AppendLine($"{match.Id} | " +
+                                               $"{match.Name} | " +
+                                               $"{(validationPair?.IsValidDynamic != null ? "OK" : "-")}");
+
+                            hash.Add(key);
+                        }
+                    }
+
+                    builder.AppendLine();
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ docs/Rules.md
@@ -0,0 +1,51 @@
+# Rules
+
+## Security.dll (1)
+
+### SEC101.DoNotExposePlaintextSecrets (43)
+ID | Name | Validation
+---| --- | ---
+SEC101/001 | DoNotExposePlaintextSecrets/HttpAuthorizationRequestHeader | OK
+SEC101/002 | DoNotExposePlaintextSecrets/GoogleOAuthCredentials | -
+SEC101/003 | DoNotExposePlaintextSecrets/GoogleApiKey | OK
+SEC101/004 | DoNotExposePlaintextSecrets/FacebookAppCredentials | OK
+SEC101/005 | DoNotExposePlaintextSecrets/SlackToken | OK
+SEC101/006 | DoNotExposePlaintextSecrets/GitHubPat | OK
+SEC101/007 | DoNotExposePlaintextSecrets/GitHubAppCredentials | -
+SEC101/008 | DoNotExposePlaintextSecrets/AwsCredentials | OK
+SEC101/009 | DoNotExposePlaintextSecrets/LinkedInCredentials | -
+SEC101/010 | DoNotExposePlaintextSecrets/SquarePat | OK
+SEC101/011 | DoNotExposePlaintextSecrets/SquareCredentials | -
+SEC101/012 | DoNotExposePlaintextSecrets/SlackWebhook | OK
+SEC101/013 | DoNotExposePlaintextSecrets/CryptographicPrivateKey | -
+SEC101/014 | DoNotExposePlaintextSecrets/FacebookAccessToken | -
+SEC101/015 | DoNotExposePlaintextSecrets/AkamaiCredentials | OK
+SEC101/016 | DoNotExposePlaintextSecrets/StripeApiKey | OK
+SEC101/017 | DoNotExposePlaintextSecrets/NpmAuthorToken | OK
+SEC101/018 | DoNotExposePlaintextSecrets/TwilioCredentials | OK
+SEC101/019 | DoNotExposePlaintextSecrets/PicaticApiKey | -
+SEC101/020 | DoNotExposePlaintextSecrets/DropboxAccessToken | OK
+SEC101/021 | DoNotExposePlaintextSecrets/DropboxAppCredentials | OK
+SEC101/022 | DoNotExposePlaintextSecrets/PayPalBraintreeAccessToken | -
+SEC101/023 | DoNotExposePlaintextSecrets/AmazonMwsAuthToken | -
+SEC101/024 | DoNotExposePlaintextSecrets/TwilioApiKey | -
+SEC101/025 | DoNotExposePlaintextSecrets/SendGridApiKey | OK
+SEC101/026 | DoNotExposePlaintextSecrets/MailgunApiKey | OK
+SEC101/027 | DoNotExposePlaintextSecrets/MailChimpApiKey | OK
+SEC101/028 | DoNotExposePlaintextSecrets/PlaintextPassword | -
+SEC101/031 | DoNotExposePlaintextSecrets/NuGetApiKey | -
+SEC101/041 | DoNotExposePlaintextSecrets/RabbitMqConnectionString | OK
+SEC101/042 | DoNotExposePlaintextSecrets/DynatraceToken | -
+SEC101/043 | DoNotExposePlaintextSecrets/ShopifySharedSecret | -
+SEC101/044 | DoNotExposePlaintextSecrets/ShopifyAccessToken | -
+SEC101/045 | DoNotExposePlaintextSecrets/PostgreSqlConnectionString | OK
+SEC101/046 | DoNotExposePlaintextSecrets/SqlConnectionString | OK
+SEC101/047 | DoNotExposePlaintextSecrets/MySqlConnectionString | OK
+SEC101/048 | DoNotExposePlaintextSecrets/CloudantConnectionString | OK
+SEC101/049 | DoNotExposePlaintextSecrets/CredentialObject | -
+SEC101/050 | DoNotExposePlaintextSecrets/MongoDbConnectionString | -
+SEC101/051 | DoNotExposePlaintextSecrets/GpgCredential | -
+SEC101/053 | DoNotExposePlaintextSecrets/GoogleServiceAccountKey | -
+SEC101/054 | DoNotExposePlaintextSecrets/AlibabaAccessKey | -
+SEC101/102 | DoNotExposePlaintextSecrets/AdoPat | -
+
