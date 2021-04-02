75ca3f58565455953f1d989d4ea95846eb22c52c
jameswinkler <77122232+jameswinkler@users.noreply.github.com>
2021-04-02 13:44:53 -0700

    Add case for PostgreSql (#354)
    * Allow redirects (30* status codes)
    
    * Some cleanup for CryptographicPrivateKey exceptions
    
    * Some corrections
    
    * Update HttpAuthorizationHeader
    
    * Additional tweak
    
    * Add additional case
    
    * Add test
    
    Co-authored-by: Eddy Nakamura <eddynaka@gmail.com>

---
+++ Src/Plugins/Tests.Security/Validators/SEC101_045.PostgreSqlConnectionStringValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validators
+{
+    public class PostgreSqlConnectionStringValidatorTests
+    {
+        private const ValidationState ExpectedValidationState = ValidationState.Unknown;
+
+        [Fact]
+        public void PostgreSqlConnectionStringValidator_Test()
+        {
+            string fingerprintText = "[host=99.9.9.99][id=accoutName][resource=database][secret=password]";
+            var fingerprint = new Fingerprint(fingerprintText);
+            string message = null;
+            var keyValuePairs = new Dictionary<string, string>();
+
+            ValidationState actualValidationState = PostgreSqlConnectionStringValidator.IsValidDynamic(ref fingerprint, ref message, ref keyValuePairs);
+            Assert.Equal(ExpectedValidationState, actualValidationState);
+        }
+    }
+}
