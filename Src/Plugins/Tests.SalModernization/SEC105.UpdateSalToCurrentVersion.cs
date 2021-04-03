13ac5bbb959e463929aad7f517f54231fe326b58
jameswinkler <77122232+jameswinkler@users.noreply.github.com>
2021-04-03 15:21:19 -0700

    Add SalModernization plugin (#347)
    * Test branch for Sal Analyzer
    
    * updating
    
    * Removing duplicated code
    
    * Add more rules, update sarif
    
    * Add more rules
    
    * Fix mismatch
    
    * Update proj file to expore JSON
    
    * Add remaining rules
    
    * Rename everything, add dummy validator, remove comment
    
    * Remove test code
    
    * cleanup project file
    
    * More proj file updates
    
    * Delete validator
    
    * Update regex with word barriers
    
    * Correct messages
    
    Co-authored-by: Eddy Nakamura <ednakamu@microsoft.com>
    Co-authored-by: Eddy Nakamura <eddynaka@gmail.com>
    Co-authored-by: Michael C. Fanning <michael.fanning@microsoft.com>

---
+++ Src/Plugins/Tests.SalModernization/SEC105.UpdateSalToCurrentVersion.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.SalModernization
+{
+    public class UpdateSalToCurrentVersionTests : EndToEndTestsSalModernization
+    {
+        public UpdateSalToCurrentVersionTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        protected override string RuleId => "SEC105";
+
+        protected override string Framework => "netstandard2.0";
+
+        [Fact]
+        public void UpdateSalToCurrentVersion_EndToEndFunctionalTests()
+            => RunAllTests();
+    }
+}
---
+++ Src/Plugins/Tests.SalModernization/TestData/UpdateSalToCurrentVersion/Inputs/SEC105_001.AnalysisAssume.cpp
@@ -0,0 +1,13 @@
+﻿#define __allocator                         _SAL_L_Source_(__allocator, (), __inner_allocator)
+#define __deallocate(kind)                  _SAL_L_Source_(__deallocate, (kind), _Pre_ __notnull __post_invalid)
+#define __deallocate_opt(kind)              _SAL_L_Source_(__deallocate_opt, (kind), _Pre_ __maybenull __post_invalid)
+
+
+
+__analysis_assume(IoControlCode == IOCTL_GPD_READ_PORT_UCHAR || \
+    IoControlCode == IOCTL_GPD_READ_PORT_USHORT || \
+    IoControlCode == IOCTL_GPD_READ_PORT_ULONG || \
+    IoControlCode == IOCTL_GPD_WRITE_PORT_UCHAR || \
+    IoControlCode == IOCTL_GPD_WRITE_PORT_USHORT || \
+
+
