769cc63fba9b42282bd7084bf4ff7ed7a5c1abb5
Eddy Nakamura <ednakamu@microsoft.com>
2021-04-13 18:07:44 -0300

    Fixing uri construction

--- Src/Plugins/Security/SEC101_001.HttpAuthorizationRequestHeaderValidator.cs
+++ Src/Plugins/Security/SEC101_001.HttpAuthorizationRequestHeaderValidator.cs
@@ -76,9 +76,9 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
                                                                 ref Dictionary<string, string> options)
         {
             string host = fingerprint.Host;
-            string scheme = fingerprint.Scheme ?? "https://";
+            string scheme = fingerprint.Scheme ?? "https";
             string path = fingerprint.Path ?? string.Empty;
-            string uri = scheme + host + path;
+            string uri = scheme + "://" + host + path;
 
             try
             {
