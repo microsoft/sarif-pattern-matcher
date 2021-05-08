117a9909c734adc464a11fcf156e2b5b0a61f425
Eddy Nakamura <eddynaka@gmail.com>
2021-05-08 18:02:48 -0300

    Analyze database command (#424)
    * Analyze database command (only kusto for now)
    
    Minor improvements
    
    Removing unused  rowCount
    
    SarifLogger should respect persistenceOptions
    
    Improving performance
    
    * Refactoring, simple docs
    
    * fixing thread count

---
+++ Src/Sarif.PatternMatcher.Cli/AnalyzeDatabaseCommand.cs
@@ -0,0 +1,158 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis.Sarif.Driver;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli.DatabaseReaders;
+using Microsoft.CodeAnalysis.Sarif.Writers;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli
+{
+    internal class AnalyzeDatabaseCommand : CommandBase
+    {
+        private const int DefaultBatchSize = 10000;
+        private readonly HashSet<string> disabledSkimmers;
+        private readonly ConcurrentDictionary<string, string> content;
+        private bool dynamicValidation;
+        private Channel<string> contentChannel;
+
+        public AnalyzeDatabaseCommand()
+        {
+            disabledSkimmers = new HashSet<string>();
+            content = new ConcurrentDictionary<string, string>();
+        }
+
+        public int Run(AnalyzeDatabaseOptions options)
+        {
+            try
+            {
+                options.BatchSize = (options.BatchSize > 0) ? options.BatchSize : DefaultBatchSize;
+                options.Threads = options.Threads > 0 ? options.Threads : Environment.ProcessorCount * 4;
+                dynamicValidation = options.DynamicValidation;
+
+                contentChannel = Channel.CreateUnbounded<string>();
+
+                AggregatingLogger aggregatingLogger = InitializeLogger(options);
+
+                var sarifLogger = new SarifLogger(
+                    options.OutputFilePath,
+                    options.ConvertToLogFilePersistenceOptions(),
+                    dataToInsert: options.DataToInsert.ToFlags(),
+                    dataToRemove: options.DataToRemove.ToFlags(),
+                    tool: MakeTool(),
+                    levels: options.Level,
+                    kinds: options.Kind);
+                aggregatingLogger.Loggers.Add(sarifLogger);
+
+                aggregatingLogger.AnalysisStarted();
+                ISet<Skimmer<AnalyzeContext>> skimmers = AnalyzeCommand.CreateSkimmersFromDefinitionsFiles(FileSystem, options.SearchDefinitionsPaths);
+
+                var workers = new Task<bool>[options.Threads];
+
+                for (int i = 0; i < options.Threads; i++)
+                {
+                    workers[i] = AnalyzeTargetAsync(aggregatingLogger, skimmers);
+                }
+
+                switch (options.ConnectionType)
+                {
+                    case Enums.ConnectionType.Kusto:
+                    {
+                        var reader = new KustoDatabaseReader(contentChannel, content);
+                        reader.Query(options);
+                        break;
+                    }
+
+                    case Enums.ConnectionType.SqlLite:
+                    {
+                        var reader = new SqlLiteDatabaseReader(contentChannel, content);
+                        reader.Query(options);
+                        break;
+                    }
+
+                    default:
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                Task.WhenAll(workers).Wait();
+
+                aggregatingLogger.AnalysisStopped(RuntimeConditions.None);
+                aggregatingLogger?.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return FAILURE;
+            }
+
+            return SUCCESS;
+        }
+
+        private static Tool MakeTool() => new Tool
+        {
+            Driver = new ToolComponent
+            {
+                Name = "Live Secrets Detection",
+            },
+        };
+
+        private async Task<bool> AnalyzeTargetAsync(IAnalysisLogger logger, IEnumerable<Skimmer<AnalyzeContext>> skimmers)
+        {
+            ChannelReader<string> reader = contentChannel.Reader;
+
+            // Wait until there is work or the channel is closed.
+            while (await reader.WaitToReadAsync())
+            {
+                // Loop while there is work to do.
+                while (reader.TryRead(out string contentName))
+                {
+                    if (content.TryRemove(contentName, out string contentData))
+                    {
+                        try
+                        {
+                            var context = new AnalyzeContext
+                            {
+                                Logger = logger,
+                                FileContents = contentData,
+                                DynamicValidation = dynamicValidation,
+                                TargetUri = new Uri(contentName, UriKind.RelativeOrAbsolute),
+                            };
+
+                            using (context)
+                            {
+                                AnalyzeCommand.AnalyzeTargetHelper(context, skimmers, disabledSkimmers);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.Error.WriteLine(e.Message);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private AggregatingLogger InitializeLogger(AnalyzeOptionsBase analyzeOptions)
+        {
+            Tool tool = MakeTool();
+            var logger = new AggregatingLogger();
+
+            if (!analyzeOptions.Quiet)
+            {
+                var consoleLogger = new ConsoleLogger(analyzeOptions.Quiet, tool.Driver.Name, analyzeOptions.Level, analyzeOptions.Kind);
+                logger.Loggers.Add(consoleLogger);
+            }
+
+            return logger;
+        }
+    }
+}
---
+++ Src/Sarif.PatternMatcher.Cli/DatabaseReaders/KustoDatabaseReader.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+using Kusto.Data.Common;
+using Kusto.Data.Net.Client;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli.DatabaseReaders
+{
+    internal class KustoDatabaseReader : BaseReader, IDatabaseReader
+    {
+        public KustoDatabaseReader(Channel<string> contentChannel, ConcurrentDictionary<string, string> content)
+            : base(contentChannel, content)
+        {
+        }
+
+        public void Query(AnalyzeDatabaseOptions options)
+        {
+            long queryRowCount;
+            string query = options.Target;
+            int batchSize = options.BatchSize;
+            string identityColumn = options.IdentityColumn;
+            string countQuery = $"{query} | count";
+
+            string tableName = query.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries)[0].Trim();
+
+            // Get the total number of rows for the target query
+            using (ICslQueryProvider provider = KustoClientFactory.CreateCslQueryProvider(options.Connection))
+            {
+                using (IDataReader reader = provider.ExecuteQuery(countQuery))
+                {
+                    reader.Read();
+                    queryRowCount = reader.GetInt64(0);
+                }
+            }
+
+            double steps = Math.Ceiling((float)queryRowCount / batchSize);
+            var batchQueries = new List<string>((int)steps);
+            for (int i = 0; i < steps; i++)
+            {
+                string batchStepQuery;
+                if (i == 0)
+                {
+                    // The current logic for the Kusto query for a single batch is as follows. This happens on the Kusto cluster as part of the query.
+                    // 1. Execute and cache the original query.
+                    // 2. Make a first result set which is the results of the original query filtered to only the identity column, ordered by the identity column, and has an additional column added which is the row number.
+                    // 3. Filter the first result set where row number is between the current batch row number range.
+                    // 4. Make a second result set that contains the entire results of the original query.
+                    // 5. Join the first and second result sets together on the identity column.
+                    batchStepQuery = $"set notruncation;let mainQuery=materialize({query}); mainQuery | project {identityColumn} | order by {identityColumn} asc | extend BatchingRowNum=row_number() | where BatchingRowNum <= {batchSize} | project-away BatchingRowNum | join (mainQuery) on {identityColumn}";
+                }
+                else
+                {
+                    batchStepQuery = $"set notruncation;let mainQuery=materialize({query}); mainQuery | project {identityColumn} | order by {identityColumn} asc | extend BatchingRowNum=row_number() | where BatchingRowNum > {i * batchSize} and BatchingRowNum <= {(i + 1) * batchSize} | project-away BatchingRowNum | join (mainQuery) on {identityColumn}";
+                }
+
+                batchQueries.Add(batchStepQuery);
+            }
+
+            var parallelOptions = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = options.Threads,
+            };
+
+            Parallel.ForEach(batchQueries, parallelOptions, (stepQuery) => ProcessBatch(options.Connection, stepQuery, identityColumn));
+
+            contentChannel.Writer.Complete();
+        }
+
+        public void ProcessBatch(string connection, string stepQuery, string identity)
+        {
+            // Discard any empty queries
+            if (string.IsNullOrWhiteSpace(stepQuery))
+            {
+                return;
+            }
+
+            // Run actual Kusto query
+            using (ICslQueryProvider provider = KustoClientFactory.CreateCslQueryProvider(connection))
+            {
+                IDataReader reader = null;
+
+                try
+                {
+                    reader = provider.ExecuteQuery(stepQuery);
+                    ReadAll(reader, identity);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    reader?.Dispose();
+                }
+            }
+        }
+    }
+}
