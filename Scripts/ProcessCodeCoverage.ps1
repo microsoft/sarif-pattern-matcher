1ef526cdc8fc274df48bf8e65abb423e5151e56e
Eddy Nakamura <ednakamu@microsoft.com>
2021-05-17 10:34:27 -0300

    Updating package versions

--- Scripts/ProcessCodeCoverage.ps1
+++ Scripts/ProcessCodeCoverage.ps1
@@ -4,7 +4,7 @@ $files = Get-ChildItem "bld\TestResults" -Filter "*.coverage" -Recurse
 
 foreach ($file in $files)
 {
-    $command = 'microsoft.codecoverage.16.8.3\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = 'microsoft.codecoverage.16.9.4\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
