95b153a3b7fd79328b88e8ac427579efaf3e01ce
Eddy Nakamura <eddynaka@gmail.com>
2021-05-17 11:04:13 -0300

    Changing code coverage (#453)
    * Changing code-coverage configuration
    
    * Updating yaml to remove -tests from reporting
    
    * Updating package versions
    
    * Fixing code coverage script

--- Scripts/ProcessCodeCoverage.ps1
+++ Scripts/ProcessCodeCoverage.ps1
@@ -1,10 +1,10 @@
-nuget install Microsoft.CodeCoverage -version 16.8.3
+nuget install Microsoft.CodeCoverage -version 16.9.4
 
 $files = Get-ChildItem "bld\TestResults" -Filter "*.coverage" -Recurse
 
 foreach ($file in $files)
 {
-    $command = 'microsoft.codecoverage.16.8.3\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = 'microsoft.codecoverage.16.9.4\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
