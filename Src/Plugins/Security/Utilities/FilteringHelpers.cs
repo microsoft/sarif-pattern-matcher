4db58996c8e5739f8af47d25f5eca9183d39ecb8
Eddy Nakamura <ednakamu@microsoft.com>
2021-05-30 09:23:45 -0300

    MySQL single line refactor

--- Src/Plugins/Security/Utilities/FilteringHelpers.cs
+++ Src/Plugins/Security/Utilities/FilteringHelpers.cs
@@ -3,9 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-
-using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Utilities
 {
