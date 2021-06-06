fe8daeba5da3a62aea79ac89fb938ace69c71f41
Eddy Nakamura <ednakamu@microsoft.com>
2021-06-05 21:51:08 -0300

    Improving ValidatingVisitor and regex

--- Src/Sarif.PatternMatcher/ValidatingVisitor.cs
+++ Src/Sarif.PatternMatcher/ValidatingVisitor.cs
@@ -81,6 +81,11 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
                 node.Message.Arguments[1] = validationPrefix;
                 node.Message.Arguments[4] = validationSuffix;
                 node.Message.Arguments[5] = SearchSkimmer.NormalizeValidatorMessage(message);
+
+                if (node.Fingerprints.ContainsKey(SearchSkimmer.AssetFingerprint))
+                {
+                    node.Fingerprints[SearchSkimmer.AssetFingerprint] = fingerprint.GetAssetFingerprintText();
+                }
             }
 
             return node;
