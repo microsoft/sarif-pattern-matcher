79a67debcaae8b3e69c43ebc6f67a2edddfae447
Eddy Nakamura <ednakamu@microsoft.com>
2021-06-29 16:44:42 -0300

    Adding dictionary constructor, updating tests

--- Src/Test.UnitTests.Sarif.PatternMatcher/ValidatingVisitorTests.cs
+++ Src/Test.UnitTests.Sarif.PatternMatcher/ValidatingVisitorTests.cs
@@ -19,86 +19,14 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
         [Fact]
         public void ValidatingVisitor_ShouldOverrideAssetFingerprint()
         {
-            TestRuleValidator.OverrideIsValidDynamic = (ref Fingerprint fingerprint,
-                                                        ref string message,
-                                                        Dictionary<string, string> options,
-                                                        ref ResultLevelKind resultLevelKind) =>
+            for (int i = 0; i < s_validatingVisitorTestCases.Length; i++)
             {
-                fingerprint.Id = "test";
-                return ValidationState.Authorized;
-            };
-
-            string validatorAssemblyPath = $@"c:\{Guid.NewGuid()}.dll";
-            string scanTargetExtension = Guid.NewGuid().ToString();
-
-            var mockFileSystem = new Mock<IFileSystem>();
-            mockFileSystem.Setup(x => x.FileExists(validatorAssemblyPath)).Returns(true);
-            mockFileSystem.Setup(x => x.AssemblyLoadFrom(validatorAssemblyPath)).Returns(this.GetType().Assembly);
-
-            var validators = new ValidatorsCache(
-                new string[] { validatorAssemblyPath },
-                fileSystem: mockFileSystem.Object);
-
-            var validatingVisitor = new ValidatingVisitor(validators);
-            validatingVisitor.VisitRun(new Run
-            {
-                Tool = new Tool
-                {
-                    Driver = new ToolComponent
-                    {
-                        Rules = new[]
-                        {
-                            new ReportingDescriptor
-                            {
-                                Id = "TestRule",
-                                Name = "TestRule"
-                            }
-                        }
-                    }
-                }
-            });
-
-            var fingerprint = new Fingerprint
-            {
-                Secret = "secret",
-                Platform = nameof(AssetPlatform.GitHub),
-            };
-
-            const string updatedAssetFingerprint = "[id=test][platform=GitHub]";
-            var updatedFingerprint = new Fingerprint(updatedAssetFingerprint);
-
-            var result = new Result
-            {
-                RuleId = "TestRule",
-                Fingerprints = new Dictionary<string, string>
-                {
-                    { SearchSkimmer.AssetFingerprint, fingerprint.GetAssetFingerprintText() },
-                    { SearchSkimmer.AssetFingerprintV2, fingerprint.GetAssetFingerprintText(jsonFormat: true) },
-                    { SearchSkimmer.ValidationFingerprint, fingerprint.GetValidationFingerprintText() },
-                    { SearchSkimmer.ValidationFingerprintV2, fingerprint.GetValidationFingerprintText(jsonFormat: true)},
-                },
-                Message = new Message
-                {
-                    Arguments = new List<string>
-                    {
-                        "secret…",
-                        "a valid ",
-                        "",
-                        "legacy format GitHub personal access token",
-                        "",
-                        " (the compromised GitHub account is '[username](https://github.com/username)' which has access to the following orgs '[None]')"
-                    }
-                }
-            };
-
-            result = validatingVisitor.VisitResult(result);
-            result.Fingerprints[SearchSkimmer.AssetFingerprint].Should().Be(updatedAssetFingerprint);
-            result.Fingerprints[SearchSkimmer.ValidationFingerprint].Should().Be(fingerprint.GetValidationFingerprintText());
-            result.Fingerprints[SearchSkimmer.AssetFingerprintV2].Should().Be(updatedFingerprint.GetAssetFingerprintText(jsonFormat: true));
+                ValidatingVisitorTestCase testCase = s_validatingVisitorTestCases[i];
+                Validate(testCase.Original, testCase.Expected);
+            }
         }
 
-        [Fact]
-        public void ValidatingVisitor_ShouldNotThrowAnExcecption()
+        private void Validate(Fingerprint original, Fingerprint expected)
         {
             TestRuleValidator.OverrideIsValidDynamic = (ref Fingerprint fingerprint,
                                                         ref string message,
@@ -139,52 +67,72 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
                 }
             });
 
-            var fingerprint = new Fingerprint
-            {
-                Host = "some-database-name.database.windows.net:1433",
-                Id = "username",
-                Part = "servers",
-                Resource = "catalog-db][host=tcp:some-database-name.database.windows.net,1433][pwd=password]\\",
-                Platform = nameof(AssetPlatform.GitHub),
-            };
-
-            var updatedFingerprint = new Fingerprint
-            {
-                Host = "some-database-name.database.windows.net:1433",
-                Id = "test", // This value was changed during the "analysis".
-                Part = "servers",
-                Resource = "catalog-db][host=tcp:some-database-name.database.windows.net,1433][pwd=password]\\",
-                Platform = nameof(AssetPlatform.GitHub),
-            };
-
             var result = new Result
             {
                 RuleId = "TestRule",
                 Fingerprints = new Dictionary<string, string>
                 {
-                    { SearchSkimmer.AssetFingerprint, fingerprint.GetAssetFingerprintText() },
-                    { SearchSkimmer.AssetFingerprintV2, fingerprint.GetAssetFingerprintText(jsonFormat: true) },
-                    { SearchSkimmer.ValidationFingerprint, fingerprint.GetValidationFingerprintText() },
-                    { SearchSkimmer.ValidationFingerprintV2, fingerprint.GetValidationFingerprintText(jsonFormat: true)},
+                    { SearchSkimmer.AssetFingerprint, original.GetAssetFingerprintText() },
+                    { SearchSkimmer.AssetFingerprintV2, original.GetAssetFingerprintText(jsonFormat: true) },
+                    { SearchSkimmer.ValidationFingerprint, original.GetValidationFingerprintText() },
+                    { SearchSkimmer.ValidationFingerprintV2, original.GetValidationFingerprintText(jsonFormat: true)},
                 },
                 Message = new Message
                 {
-                    Arguments = new List<string>
-                    {
-                        "secret…",
-                        "a valid ",
-                        "",
-                        "legacy format GitHub personal access token",
-                        "",
-                        " (the compromised GitHub account is '[username](https://github.com/username)' which has access to the following orgs '[None]')"
-                    }
+                    Arguments = new List<string> { "", "", "", "", "", "" }
                 }
             };
 
             result = validatingVisitor.VisitResult(result);
-            result.Fingerprints[SearchSkimmer.AssetFingerprint].Should().Be(updatedFingerprint.GetAssetFingerprintText());
-            result.Fingerprints[SearchSkimmer.ValidationFingerprint].Should().Be(fingerprint.GetValidationFingerprintText());
-            result.Fingerprints[SearchSkimmer.AssetFingerprintV2].Should().Be(updatedFingerprint.GetAssetFingerprintText(jsonFormat: true));
+
+            // AssetFingerprint should be updated.
+            result.Fingerprints[SearchSkimmer.AssetFingerprint].Should().Be(expected.GetAssetFingerprintText());
+            result.Fingerprints[SearchSkimmer.AssetFingerprintV2].Should().Be(expected.GetAssetFingerprintText(jsonFormat: true));
+
+            // ValidationFingerprint should be the same as original.
+            result.Fingerprints[SearchSkimmer.ValidationFingerprint].Should().Be(original.GetValidationFingerprintText());
+            result.Fingerprints[SearchSkimmer.ValidationFingerprintV2].Should().Be(original.GetValidationFingerprintText(jsonFormat: true));
+        }
+
+        private static readonly ValidatingVisitorTestCase[] s_validatingVisitorTestCases = new[]
+        {
+            new ValidatingVisitorTestCase
+            {
+                Original = new Fingerprint
+                {
+                    Secret = "secret",
+                    Platform = nameof(AssetPlatform.GitHub),
+                },
+                Expected = new Fingerprint
+                {
+                    Id = "test",
+                    Secret = "secret",
+                    Platform = nameof(AssetPlatform.GitHub),
+                },
+                Title = "Simple override test."
+            },
+            new ValidatingVisitorTestCase
+            {
+                Original = new Fingerprint
+                {
+                    Secret = "secret[id=id]",
+                    Platform = nameof(AssetPlatform.GitHub),
+                },
+                Expected = new Fingerprint
+                {
+                    Id = "test",
+                    Secret = "secret[id=id]",
+                    Platform = nameof(AssetPlatform.GitHub),
+                },
+                Title = "Broken square bracket fingerprint."
+            }
+        };
+
+        internal struct ValidatingVisitorTestCase
+        {
+            public string Title;
+            public Fingerprint Original;
+            public Fingerprint Expected;
         }
     }
 }
