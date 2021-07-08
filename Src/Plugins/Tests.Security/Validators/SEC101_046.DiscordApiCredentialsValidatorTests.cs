1a91af8d6f8604a65878c38e9a23af9086080baf
David Camp <Bpendragon@users.noreply.github.com>
2021-07-08 13:14:57 -0700

    Added tests for Discord API Credentials

---
+++ Src/Plugins/Tests.Security/Validators/SEC101_046.DiscordApiCredentialsValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validators
+{
+    public class DiscordApiCredentialsValidatorTests
+    {
+        [Fact]
+        public void DiscordApiCredentialsValidator_Test()
+        {
+            string fingerprintText = "";
+            if (string.IsNullOrEmpty(fingerprintText))
+            {
+                return;
+            }
+
+            string message = null;
+            ResultLevelKind resultLevelKind = default;
+            var fingerprint = new Fingerprint(fingerprintText);
+            var keyValuePairs = new Dictionary<string, string>();
+
+            DiscordApiCredentialsValidator.IsValidDynamic(ref fingerprint,
+                                               ref message,
+                                               keyValuePairs,
+                                               ref resultLevelKind);
+        }
+    }
+}
