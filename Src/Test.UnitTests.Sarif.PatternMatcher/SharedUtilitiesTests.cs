a5e4da4e7aa0cb010c90de41b90e1a40fd2ec1c8
David Camp <Bpendragon@users.noreply.github.com>
2021-07-20 15:35:12 -0700

    fix import order to align with other files.

--- Src/Test.UnitTests.Sarif.PatternMatcher/SharedUtilitiesTests.cs
+++ Src/Test.UnitTests.Sarif.PatternMatcher/SharedUtilitiesTests.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+
 using FluentAssertions;
 
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
-using System.Collections.Generic;
 
 using Xunit;
 
