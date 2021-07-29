f77d069cf5ec5a1cf9c0c02ebb6aa643a8b66b58
David Camp <Bpendragon@users.noreply.github.com>
2021-07-29 11:54:43 -0700

    Rebaseline tests
    Ran BuildAndTest.cmd

--- Src/Plugins/Security/SEC101_011.SquareCredentialsValidator.cs
+++ Src/Plugins/Security/SEC101_011.SquareCredentialsValidator.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
+
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
 using Microsoft.RE2.Managed;
 
