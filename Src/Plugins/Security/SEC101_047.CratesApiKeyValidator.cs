39ad20acceb9352b73839007ab9529363a692513
Eddy Nakamura <ednakamu@microsoft.com>
2021-08-10 11:42:23 -0300

    Adding CratesApiKey validator

---
+++ Src/Plugins/Security/SEC101_047.CratesApiKeyValidator.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+using Microsoft.RE2.Managed;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
+{
+    public class CratesApiKeyValidator : ValidatorBase
+    {
+        internal static CratesApiKeyValidator Instance;
+
+        static CratesApiKeyValidator()
+        {
+            Instance = new CratesApiKeyValidator();
+        }
+
+        public static IEnumerable<ValidationResult> IsValidStatic(Dictionary<string, FlexMatch> groups)
+        {
+            return IsValidStatic(Instance, groups);
+        }
+
+        public static ValidationState IsValidDynamic(ref Fingerprint fingerprint,
+                                                     ref string message,
+                                                     Dictionary<string, string> options,
+                                                     ref ResultLevelKind resultLevelKind)
+        {
+            return IsValidDynamic(Instance,
+                                  ref fingerprint,
+                                  ref message,
+                                  options,
+                                  ref resultLevelKind);
+        }
+
+        protected override IEnumerable<ValidationResult> IsValidStaticHelper(Dictionary<string, FlexMatch> groups)
+        {
+            if (!groups.TryGetNonEmptyValue("secret", out FlexMatch secret))
+            {
+                return ValidationResult.CreateNoMatch();
+            }
+
+            if (!ContainsDigitAndChar(secret.Value))
+            {
+                return ValidationResult.CreateNoMatch();
+            }
+
+            var validationResult = new ValidationResult
+            {
+                Fingerprint = new Fingerprint
+                {
+                    Secret = secret.Value,
+                    Platform = nameof(AssetPlatform.Crates),
+                },
+                ValidationState = ValidationState.Unknown,
+            };
+
+            return new[] { validationResult };
+        }
+
+        protected override ValidationState IsValidDynamicHelper(ref Fingerprint fingerprint,
+                                                                ref string message,
+                                                                Dictionary<string, string> options,
+                                                                ref ResultLevelKind resultLevelKind)
+        {
+            string secret = fingerprint.Secret;
+
+            const string uri = "https://crates.io/api/v1/crates/sarif-pattern-matcher/owners";
+
+            try
+            {
+                HttpClient client = CreateOrRetrieveCachedHttpClient();
+
+                using var request = new HttpRequestMessage(HttpMethod.Delete, uri);
+                request.Headers.Add("Authorization", secret);
+
+                using HttpResponseMessage response = client
+                    .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                    .GetAwaiter()
+                    .GetResult();
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    {
+                        return ValidationState.Authorized;
+                    }
+
+                    case HttpStatusCode.Forbidden:
+                    {
+                        return ValidationState.Unauthorized;
+                    }
+
+                    default:
+                    {
+                        return ReturnUnexpectedResponseCode(ref message, response.StatusCode);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return ReturnUnhandledException(ref message, e);
+            }
+        }
+    }
+}
