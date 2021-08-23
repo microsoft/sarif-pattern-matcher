b74bc7869ea4294e3172786e0a3e7d81cdf9b25d
David Camp <Bpendragon@users.noreply.github.com>
2021-08-23 10:27:45 -0700

    Whitespace change to force new CI run.

--- Src/Plugins/Tests.Security/Validators/SEC101_044.NpmCredentialsValidatorTests.cs
+++ Src/Plugins/Tests.Security/Validators/SEC101_044.NpmCredentialsValidatorTests.cs
@@ -110,7 +110,6 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validator
 
                 mockHandler.Clear();
             }
-
             sb.Length.Should().Be(0, sb.ToString());
         }
     }
