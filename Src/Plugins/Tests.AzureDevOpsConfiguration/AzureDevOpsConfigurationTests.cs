50f422b138d8db1d81ce3292d95b7afb76d2cea7
Yong Yan <yanyong-gh@outlook.com>
2021-09-07 17:27:27 -0700

    Add E2E tests

---
+++ Src/Plugins/Tests.AzureDevOpsConfiguration/AzureDevOpsConfigurationTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.AzureDevOpsConfiguration
+{
+    public class AzureDevOpsConfigurationTests : EndToEndTestsAzureDevOpsConfiguration
+    {
+        public AzureDevOpsConfigurationTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        protected override string RuleId => "AZC101";
+
+        protected override string Framework => "netstandard2.0";
+
+        [Fact]
+        public void AzureDevOpsConfiguration_EndToEndFunctionalTests()
+            => RunAllTests();
+    }
+}
---
+++ Src/Plugins/Tests.AzureDevOpsConfiguration/EndToEndTestsAzureDevOpsConfiguration.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security;
+
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.AzureDevOpsConfiguration
+{
+    public abstract class EndToEndTestsAzureDevOpsConfiguration : EndToEndTests
+    {
+        public EndToEndTestsAzureDevOpsConfiguration(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        protected override string TestLogResourceNameRoot => $"Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.AzureDevOpsConfiguration.TestData.{TypeUnderTest}";
+
+        protected override string ProductDirectory
+        {
+            get
+            {
+                string path = typeof(EndToEndTestsAzureDevOpsConfiguration).Assembly.Location;
+                path = GitHelper.Default.GetTopLevel(path);
+                return Path.Combine(path, @"src\Plugins\Tests.AzureDevOpsConfiguration");
+            }
+        }
+    }
+}
