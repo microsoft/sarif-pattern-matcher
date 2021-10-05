e29673262bdbb2a4199421c65c5847b5650cb336
Yong Yan <yanyong-gh@outlook.com>
2021-10-05 12:47:28 -0700

    Add AzureDevOps configuration scanner plugin (#547)
    * Add AzureDevOps configuration scanner plugin
    
    * Update file name regex to match api uri
    
    * Update rule json - define top level general rules.
    
    * Update regex
    
    * Add E2E tests
    
    * fix test result file
    
    * - Separate rule json definition file.
    - Parameterize message with arguments
    
    * Add AzureDevOps organization/project information in result message
    
    * add support to construct result message using intrafile match data
    
    * - update rule name
    - message format
    
    * updating to netstandard2.1 to be similar to others.
    
    * Updating tests
    
    Co-authored-by: Eddy Nakamura <eddynaka@gmail.com>
    Co-authored-by: Eddy Nakamura <ednakamu@microsoft.com>

---
+++ Src/Plugins/Tests.AzureDevOpsConfiguration/AZC101.BuildDefinitionSecurityTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.AzureDevOpsConfiguration
+{
+    public class BuildDefinitionSecurityTests : EndToEndTestsAzureDevOpsConfiguration
+    {
+        public BuildDefinitionSecurityTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        protected override string RuleId => "AZC101";
+
+        protected override string Framework => "netstandard2.1";
+
+        [Fact]
+        public void AzureDevOpsConfiguration_EndToEndFunctionalTests()
+            => RunAllTests();
+    }
+}
