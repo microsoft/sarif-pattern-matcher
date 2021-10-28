0c0d8653a0f890d78c0ea87f4fbcc785fe31a5f6
Yong Yan <yanyong-gh@outlook.com>
2021-10-27 18:00:01 -0700

    Fix fingerprint to have unique fingerprint entities with same org/project/entitiy id

---
+++ Src/Plugins/AzureDevOpsConfiguration/AZC102_150.DoNotUseClassicConnections.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+using Microsoft.RE2.Managed;
+
+using Newtonsoft.Json;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.AzureDevOpsConfiguration
+{
+    public class DoNotUseClassicConnectionsValidator : StaticValidatorBase
+    {
+        protected override IEnumerable<ValidationResult> IsValidStaticHelper(IDictionary<string, FlexMatch> groups)
+        {
+            if (!groups.TryGetNonEmptyValue("org", out FlexMatch org) ||
+                !groups.TryGetNonEmptyValue("project", out FlexMatch project) ||
+                !groups.TryGetNonEmptyValue("serviceConnectionId", out FlexMatch serviceConnectionId) ||
+                !groups.TryGetNonEmptyValue("secret", out FlexMatch secret))
+            {
+                return ValidationResult.CreateNoMatch();
+            }
+
+            var validationResult = new ValidationResult
+            {
+                Fingerprint = new Fingerprint
+                {
+                    Host = org.Value,
+                    Resource = project.Value,
+                    Id = serviceConnectionId.Value,
+                    Platform = nameof(AssetPlatform.AzureDevOps),
+                },
+                RegionFlexMatch = secret,
+                ValidationState = ValidationState.Authorized,
+            };
+            return new[] { validationResult };
+        }
+    }
+}
