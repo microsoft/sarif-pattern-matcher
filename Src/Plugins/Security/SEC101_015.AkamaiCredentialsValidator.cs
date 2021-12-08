26af1265fcfaea08d95515cfc06c1127e38781c7
Eddy Nakamura <eddynaka@gmail.com>
2021-12-08 14:17:20 -0800

    Updating NetAnalyzers package (#584)
    Co-authored-by: Michael C. Fanning <michael.fanning@microsoft.com>

--- Src/Plugins/Security/SEC101_015.AkamaiCredentialsValidator.cs
+++ Src/Plugins/Security/SEC101_015.AkamaiCredentialsValidator.cs
@@ -31,58 +31,6 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
             return request;
         }
 
-        private static string GetRequestData(string method, Uri uri)
-        {
-            string headers = string.Empty;
-            string bodyHash = string.Empty;
-
-            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t",
-                method.ToUpper(),
-                uri.Scheme,
-                uri.Host,
-                uri.PathAndQuery,
-                headers,
-                bodyHash);
-        }
-
-        private static string GetAuthDataValue(string id,
-                                               string resource,
-                                               string timestamp,
-                                               string scanIdentityGuid)
-        {
-            return string.Format("{0} client_token={1};access_token={2};timestamp={3};nonce={4};",
-                "EG1-HMAC-SHA256",
-                id,
-                resource,
-                timestamp,
-                scanIdentityGuid.ToLower());
-        }
-
-        private static string GetAuthorizationHeaderValue(string secret,
-                                                          string timestamp,
-                                                          string authData,
-                                                          string requestData)
-        {
-            string hashType = "HMACSHA256";
-
-            byte[] time = Encoding.UTF8.GetBytes(timestamp);
-            string signingKey = Convert.ToBase64String(ComputeKeyedHash(time, secret, hashType));
-
-            byte[] data = Encoding.UTF8.GetBytes(string.Format("{0}{1}", requestData, authData));
-            string authSignature = Convert.ToBase64String(ComputeKeyedHash(data, signingKey, hashType));
-
-            return string.Format("{0}signature={1}", authData, authSignature);
-        }
-
-        private static byte[] ComputeKeyedHash(byte[] data, string key, string hashType)
-        {
-            using (var algorithm = HMAC.Create(hashType.ToString()))
-            {
-                algorithm.Key = Encoding.UTF8.GetBytes(key);
-                return algorithm.ComputeHash(data);
-            }
-        }
-
         protected override IEnumerable<ValidationResult> IsValidStaticHelper(IDictionary<string, FlexMatch> groups)
         {
             FlexMatch id = groups["id"];
@@ -132,7 +80,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
             {
                 HttpClient httpClient = CreateOrRetrieveCachedHttpClient();
 
-                using var request = GenerateRequestMessage(id, host, secret, resource, scanIdentityGuid, datetime);
+                using HttpRequestMessage request = GenerateRequestMessage(id, host, secret, resource, scanIdentityGuid, datetime);
 
                 using HttpResponseMessage httpResponse = httpClient
                     .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
@@ -155,6 +103,58 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
             catch (Exception e)
             {
                 return ReturnUnhandledException(ref message, e);
+            }
+        }
+
+        private static string GetRequestData(string method, Uri uri)
+        {
+            string headers = string.Empty;
+            string bodyHash = string.Empty;
+
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t",
+                method.ToUpper(),
+                uri.Scheme,
+                uri.Host,
+                uri.PathAndQuery,
+                headers,
+                bodyHash);
+        }
+
+        private static string GetAuthDataValue(string id,
+                                               string resource,
+                                               string timestamp,
+                                               string scanIdentityGuid)
+        {
+            return string.Format("{0} client_token={1};access_token={2};timestamp={3};nonce={4};",
+                "EG1-HMAC-SHA256",
+                id,
+                resource,
+                timestamp,
+                scanIdentityGuid.ToLower());
+        }
+
+        private static string GetAuthorizationHeaderValue(string secret,
+                                                          string timestamp,
+                                                          string authData,
+                                                          string requestData)
+        {
+            string hashType = "HMACSHA256";
+
+            byte[] time = Encoding.UTF8.GetBytes(timestamp);
+            string signingKey = Convert.ToBase64String(ComputeKeyedHash(time, secret, hashType));
+
+            byte[] data = Encoding.UTF8.GetBytes(string.Format("{0}{1}", requestData, authData));
+            string authSignature = Convert.ToBase64String(ComputeKeyedHash(data, signingKey, hashType));
+
+            return string.Format("{0}signature={1}", authData, authSignature);
+        }
+
+        private static byte[] ComputeKeyedHash(byte[] data, string key, string hashType)
+        {
+            using (var algorithm = HMAC.Create(hashType.ToString()))
+            {
+                algorithm.Key = Encoding.UTF8.GetBytes(key);
+                return algorithm.ComputeHash(data);
             }
         }
     }
