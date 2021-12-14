ba5507a4505b670b5f4deb7feb231f986bf32b8b
Mary Martin <marymart@microsoft.com>
2021-12-13 23:24:01 -0800

    Added regex for new npm secret format

--- Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_017.NpmAuthorToken.ps1
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_017.NpmAuthorToken.ps1
@@ -1,3 +1,7 @@
 npm: "338a0fd3-7378-4ce1-8432-bbfc0f6777ca"
 
 <npm>338a0fd3-7378-4ce1-8432-bbfc0f6777cb</npm>
+
+npm_0dead12Test345DeadTest6789testtest01
+
+"npm_0dead12Test345DeadTest6789testtest02"
