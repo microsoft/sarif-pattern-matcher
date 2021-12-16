cc568ed3f800ae1a2a674f8467f2647610a69df2
Mary Martin <marymart@microsoft.com>
2021-12-16 15:18:41 -0800

    Added checksum validation

--- Src/Plugins/Security/SEC101_050.IdentifiableNpmAuthorTokenValidator.cs
+++ Src/Plugins/Security/SEC101_050.IdentifiableNpmAuthorTokenValidator.cs
@@ -3,9 +3,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
 using Microsoft.RE2.Managed;
@@ -16,21 +18,38 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
 {
     public class IdentifiableNpmAuthorTokenValidator : DynamicValidatorBase
     {
+        [ThreadStatic]
+        private static StringBuilder s_sb;
+
         protected override IEnumerable<ValidationResult> IsValidStaticHelper(IDictionary<string, FlexMatch> groups)
         {
             FlexMatch secret = groups["secret"];
 
-            var validationResult = new ValidationResult
+            if (groups.TryGetNonEmptyValue("checksum", out FlexMatch checksum))
             {
-                Fingerprint = new Fingerprint
-                {
-                    Secret = secret.Value,
-                    Platform = nameof(AssetPlatform.Npm),
-                },
-                ValidationState = ValidationState.Unknown,
-            };
+                string randomPart = secret.Value.String.Substring(4, 30);
+                uint checksumValue = Crc32.Calculate(randomPart);
+                string encodedChecksum = Base62EncodeUint32(checksumValue);
 
-            return new[] { validationResult };
+                if (checksum.Value != encodedChecksum)
+                {
+                    return ValidationResult.CreateNoMatch();
+                }
+
+                var validationResult = new ValidationResult
+                {
+                    Fingerprint = new Fingerprint
+                    {
+                        Secret = secret.Value,
+                        Platform = nameof(AssetPlatform.Npm),
+                    },
+                    ValidationState = ValidationState.Unknown,
+                };
+
+                return new[] { validationResult };
+            }
+
+            return ValidationResult.CreateNoMatch();
         }
 
         protected override ValidationState IsValidDynamicHelper(ref Fingerprint fingerprint,
@@ -44,6 +63,31 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
                                                        ref message,
                                                        ref resultLevelKind,
                                                        client);
+        }
+
+        private static string Base62EncodeUint32(uint value, int minimumLength = 6)
+        {
+            const string primitives = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            if (value <= 0)
+            {
+                return new string(primitives[0], minimumLength);
+            }
+
+            s_sb ??= new StringBuilder();
+            s_sb.Clear();
+
+            while (value > 0)
+            {
+                s_sb.Append(primitives[(int)(value % 62)]);
+                value /= 62;
+            }
+
+            string result = new string(s_sb.ToString().Reverse().ToArray());
+
+            return result.Length >= minimumLength
+                    ? result
+                    : new string(primitives[0], minimumLength - s_sb.Length) + result;
         }
     }
 }
