3dcb1efe008ed39c100a0b301182f83654c1faf0
Mary Martin <marymart@microsoft.com>
2022-01-11 16:12:01 -0800

    Update to use the CustomAlphabetEncoder class to encode the npm checksum

--- Src/Plugins/Security/SEC101_050.IdentifiableNpmAuthorTokenValidator.cs
+++ Src/Plugins/Security/SEC101_050.IdentifiableNpmAuthorTokenValidator.cs
@@ -3,16 +3,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
 using Microsoft.RE2.Managed;
-
-using Newtonsoft.Json;
+using Microsoft.Security.Utilities;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
 {
@@ -24,32 +20,31 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
         protected override IEnumerable<ValidationResult> IsValidStaticHelper(IDictionary<string, FlexMatch> groups)
         {
             FlexMatch secret = groups["secret"];
+            FlexMatch checksum = groups["checksum"];
 
-            if (groups.TryGetNonEmptyValue("checksum", out FlexMatch checksum))
+            string randomPart = secret.Value.String.Substring(4, 30);
+            uint checksumValue = Crc32.Calculate(randomPart);
+
+            // using the default CustomAlphabetEncoder will default to the base 62 encoding required
+            var encoder = new CustomAlphabetEncoder();
+            string encodedChecksum = encoder.Encode(checksumValue);
+
+            if (checksum.Value != encodedChecksum)
             {
-                string randomPart = secret.Value.String.Substring(4, 30);
-                uint checksumValue = Crc32.Calculate(randomPart);
-                string encodedChecksum = Base62EncodeUint32(checksumValue);
-
-                if (checksum.Value != encodedChecksum)
-                {
-                    return ValidationResult.CreateNoMatch();
-                }
-
-                var validationResult = new ValidationResult
-                {
-                    Fingerprint = new Fingerprint
-                    {
-                        Secret = secret.Value,
-                        Platform = nameof(AssetPlatform.Npm),
-                    },
-                    ValidationState = ValidationState.Unknown,
-                };
-
-                return new[] { validationResult };
+                return ValidationResult.CreateNoMatch();
             }
 
-            return ValidationResult.CreateNoMatch();
+            var validationResult = new ValidationResult
+            {
+                Fingerprint = new Fingerprint
+                {
+                    Secret = secret.Value,
+                    Platform = nameof(AssetPlatform.Npm),
+                },
+                ValidationState = ValidationState.Unknown,
+            };
+
+            return new[] { validationResult };
         }
 
         protected override ValidationState IsValidDynamicHelper(ref Fingerprint fingerprint,
@@ -63,31 +58,6 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
                                                        ref message,
                                                        ref resultLevelKind,
                                                        client);
-        }
-
-        private static string Base62EncodeUint32(uint value, int minimumLength = 6)
-        {
-            const string primitives = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-            if (value <= 0)
-            {
-                return new string(primitives[0], minimumLength);
-            }
-
-            s_sb ??= new StringBuilder();
-            s_sb.Clear();
-
-            while (value > 0)
-            {
-                s_sb.Append(primitives[(int)(value % 62)]);
-                value /= 62;
-            }
-
-            string result = new string(s_sb.ToString().Reverse().ToArray());
-
-            return result.Length >= minimumLength
-                    ? result
-                    : new string(primitives[0], minimumLength - s_sb.Length) + result;
         }
     }
 }
