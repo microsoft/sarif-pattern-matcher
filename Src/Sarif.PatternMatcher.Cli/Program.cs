54875104017f3dad4fa565f4ac37cdf4654f3ca0
Michael C. Fanning <mikefan@microsoft.com>
2022-06-13 07:55:21 -0700

    Enable response file parsing in driver.

--- Src/Sarif.PatternMatcher.Cli/Program.cs
+++ Src/Sarif.PatternMatcher.Cli/Program.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -20,6 +22,15 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli
         {
             Console.OutputEncoding = Encoding.UTF8;
 
+            args = EntryPointUtilities.GenerateArguments(args, FileSystem, new EnvironmentVariables());
+
+            bool isValidHelpCommand =
+                args.Length > 0 &&
+                args[0] == "help" &&
+                ((args.Length == 2 && IsValidVerbName(args[1])) || args.Length == 1);
+
+            bool isVersionCommand = args[0] == "version" && args.Length == 1;
+
             return Parser.Default.ParseArguments<
                 AnalyzeOptions,
                 AnalyzeDatabaseOptions,
@@ -34,7 +45,19 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli
                 (ExportRulesMetatadaOptions options) => new ExportRulesMetatadaCommand().Run(options),
                 (ExportSearchDefinitionsOptions options) => new ExportSearchDefinitionsCommand().Run(options),
                 (ValidateOptions options) => new ValidateCommand().Run(options),
-                _ => CommandBase.FAILURE);
+                _ => isValidHelpCommand || isVersionCommand
+                        ? CommandBase.SUCCESS
+                        : CommandBase.FAILURE);
+        }
+
+        private static bool IsValidVerbName(string verb)
+        {
+            return
+                verb == "analyze" ||
+                verb == "analyze-database" ||
+                verb == "export-rules" ||
+                verb == "export-search-definitions" ||
+                verb == "import-analyze";
         }
     }
 }
