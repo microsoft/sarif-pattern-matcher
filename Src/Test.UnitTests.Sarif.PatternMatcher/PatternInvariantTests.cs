085e2e16586f8f9cb87075257b96d5119f24c824
Michael C. Fanning <mikefan@microsoft.com>
2022-09-07 09:50:29 -0700

    Remove IO exception/other handling from test helpers.

--- Src/Test.UnitTests.Sarif.PatternMatcher/PatternInvariantTests.cs
+++ Src/Test.UnitTests.Sarif.PatternMatcher/PatternInvariantTests.cs
@@ -7,8 +7,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-using FluentAssertions;
-
 using Newtonsoft.Json.Linq;
 
 using Xunit;
@@ -24,38 +22,27 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
     {
         public static void VerifyAllValidatorsExist(string definitionsFileName, Assembly assembly)
         {
-            try
+            string content = File.ReadAllText(definitionsFileName);
+
+            var jObject = (JObject)JToken.Parse(content);
+
+            IEnumerable<string> rules = jObject["Definitions"][0]["MatchExpressions"].Select(x => x["Name"].ToString().Split('/')[1]).Distinct();
+
+            // Not all validators are subclasses of ValidatorBase, so for the time being, we'll have to identify them by name
+            var validators = assembly.GetTypes().Where(x => x.Name.EndsWith("Validator")).Select(x => x.Name).ToHashSet();
+
+            var rulesWithoutValidators = new List<string>();
+
+            foreach (string rule in rules)
             {
-                string content = File.ReadAllText(definitionsFileName);
-
-                var jObject = (JObject)JToken.Parse(content);
-
-                IEnumerable<string> rules = jObject["Definitions"][0]["MatchExpressions"].Select(x => x["Name"].ToString().Split('/')[1]).Distinct();
-
-                // Not all validators are subclasses of ValidatorBase, so for the time being, we'll have to identify them by name
-                var validators = assembly.GetTypes().Where(x => x.Name.EndsWith("Validator")).Select(x => x.Name).ToHashSet();
-
-                var rulesWithoutValidators = new List<string>();
-
-                foreach (string rule in rules)
+                if (!validators.TryGetValue(rule + "Validator", out string _))
                 {
-                    if (!validators.TryGetValue(rule + "Validator", out string _))
-                    {
-                        rulesWithoutValidators.Add(rule);
-                    }
+                    rulesWithoutValidators.Add(rule);
                 }
+            }
 
-                // Assert.Empty doesn't allow custom messages, so use Assert.True
-                Assert.True(rulesWithoutValidators.Count == 0, "Unable to find validators for these rules: " + string.Join(',', rulesWithoutValidators));
-            }
-            catch (IOException ioe)
-            {
-                Assert.True(false, "Failed to read the rules file.  Exception was: " + ioe.Message);
-            }
-            catch (NullReferenceException)
-            {
-                Assert.True(false, "Unexpected JSON structure in rules file");
-            }
+            // Assert.Empty doesn't allow custom messages, so use Assert.True
+            Assert.True(rulesWithoutValidators.Count == 0, "Unable to find validators for these rules: " + string.Join(',', rulesWithoutValidators));
         }
 
         public static void VerifyAllSharedStringsExist(string definitionsFilePath, string sharedStringsFilePath)
@@ -87,36 +74,24 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
 
         public static void VerifyAllTestsExist(Assembly validatorsAssembly, Assembly testsAssembly)
         {
-            try
+            // Not all validators are subclasses of ValidatorBase, so for the time being, we'll have to identify them by name
+            var validators = validatorsAssembly.GetTypes().Where(x => x.Name.EndsWith("Validator")).Select(x => x.Name).ToHashSet();
+            var tests = testsAssembly.GetTypes().Where(x => x.Name.EndsWith("ValidatorTests")).Select(x => x.Name).ToHashSet();
+
+            var rulesWithoutTests = new List<string>();
+
+            foreach (string validator in validators)
             {
-                // Not all validators are subclasses of ValidatorBase, so for the time being, we'll have to identify them by name
-                var validators = validatorsAssembly.GetTypes().Where(x => x.Name.EndsWith("Validator")).Select(x => x.Name).ToHashSet();
-                var tests = testsAssembly.GetTypes().Where(x => x.Name.EndsWith("ValidatorTests")).Select(x => x.Name).ToHashSet();
-
-                var rulesWithoutTests = new List<string>();
-
-                foreach (string validator in validators)
+                if (!tests.TryGetValue(validator + "Tests", out string _))
                 {
-                    if (!tests.TryGetValue(validator + "Tests", out string _))
-                    {
-                        // Skip Template Validators
-                        if (validator.Contains("Template")) { continue; }
+                    // Skip Template Validators
+                    if (validator.Contains("Template")) { continue; }
 
-                        rulesWithoutTests.Add(validator);
-                    }
+                    rulesWithoutTests.Add(validator);
                 }
-                // Assert.Empty doesn't allow custom messages, so use Assert.True
-                Assert.True(rulesWithoutTests.Count == 0, "Unable to find tests for these rule validators: " + string.Join(',', rulesWithoutTests));
             }
-            catch (IOException ioe)
-            {
-                Assert.True(false, "Failed to read the rules file.  Exception was: " + ioe.Message);
-            }
-            catch (NullReferenceException)
-            {
-                Assert.True(false, "Unexpected JSON structure in rules file");
-            }
-
+            // Assert.Empty doesn't allow custom messages, so use Assert.True
+            Assert.True(rulesWithoutTests.Count == 0, "Unable to find tests for these rule validators: " + string.Join(',', rulesWithoutTests));
         }
     }
 }
