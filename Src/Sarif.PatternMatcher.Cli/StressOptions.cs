7147888e7cb6948b575e5d55129c2f93ce552b7c
Hulon Jenkins <109993038+HulonJenkins@users.noreply.github.com>
2022-10-11 14:39:56 +0000

    Performance fix to match Legacy speed (#672)
    * Create cache for UTF8/16 index map when invoking patterns with groups.
    
    * Disable perf-relevant unit test.
    
    * Ensure core scanner utilizes the UTF8/UTF16 index map when scanning.
    
    * Improve test reporting.
    
    * FInal API fix.
    
    * Code clean-up
    
    * Fix logic problem in single line regex match.
    
    * Added expression8 and buffer to context to avoid creation every time
    
    * add FileSizeVsTime option to run and output file size vs runtime csv
    
    * Add timing run capability to stress testing
    
    * Add clarification comment
    
    * Add csv command line input to timing tests
    
    * Added dictionary to properly cache data during runs.
    
    * Dispose of dictionary
    
    * Updated Dictionary to speed up string comparison
    
    * Comment updates
    Variable name change
    
    * Comment updates
    Variable name change
    
    Co-authored-by: Michael C. Fanning <mikefan@microsoft.com>

--- Src/Sarif.PatternMatcher.Cli/StressOptions.cs
+++ Src/Sarif.PatternMatcher.Cli/StressOptions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+
 using CommandLine;
 
 using Microsoft.CodeAnalysis.Sarif.Driver;
@@ -10,5 +12,38 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Cli
     [Verb("stress", HelpText = "Run various stress scenarios.")]
     internal class StressOptions : SingleFileOptionsBase
     {
+        [Value(
+            0,
+            HelpText = "A naive performance or stress scenario name (currently Statelessness or RE2Timing).",
+            Required = true)]
+        public StressScenario Scenario { get; set; }
+
+        [Option(
+            "iterations",
+            HelpText = "The # of iterations to run of the specified scenario.",
+            Default = 1
+            )]
+        public int Iterations { get; set; }
+
+        [Option(
+            'd',
+            "search-definitions",
+            Separator = ';',
+            Required = true,
+            HelpText = "A path to a file containing one or more search definitions to drive analysis.")]
+        public IEnumerable<string> SearchDefinitionsPaths { get; set; }
+
+        [Option(
+            "csv",
+            HelpText = "A path to create a csv file for outputs.")]
+
+        public string CSVFilePath { get; set; }
+    }
+
+    public enum StressScenario
+    {
+        Statelessness = 0,
+        RE2Timing,
+        FileSizeVsTime,
     }
 }
