2286ef90a3b37af7cd319e78f67b592072cad7ec
Michael C. Fanning <mikefan@microsoft.com>
2022-11-03 15:39:36 -0700

    Add renamed files.

---
+++ Src/Plugins/Tests.Security/SecurePlaintextSecretsValidators/SEC101_017.NpmLegacyAuthorTokenValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+using FluentAssertions;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Helpers;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validators
+{
+    /// <summary>
+    /// Testing SEC101/017.NpmLegacyAuthorTokenValidator
+    /// </summary
+    public class NpmLegacyAuthorTokenValidatorTests
+    {
+        [Fact]
+        public void NpmLegacyAuthorTokenValidator_MockHttpTests()
+        {
+            HttpMockTestCase[] testCases = NpmLegacyAuthorTokenTestCases.CreateTestCases(out Fingerprint fingerprint);
+            var sb = new StringBuilder();
+            var NpmLegacyAuthorTokenValidator = new NpmLegacyAuthorTokenValidator();
+            var mockHandler = new HttpMockHelper();
+
+            foreach (HttpMockTestCase testCase in testCases)
+            {
+                for (int i = 0; i < testCase.HttpRequestMessages.Count; i++)
+                {
+                    mockHandler.Mock(testCase.HttpRequestMessages[i], testCase.HttpResponseMessages[i]);
+                }
+
+                string message = string.Empty;
+                ResultLevelKind resultLevelKind = default;
+                var keyValuePairs = new Dictionary<string, string>();
+
+                using var httpClient = new HttpClient(mockHandler);
+                NpmLegacyAuthorTokenValidator.SetHttpClient(httpClient);
+                ValidationState currentState = NpmLegacyAuthorTokenValidator.IsValidDynamic(ref fingerprint,
+                                                                                      ref message,
+                                                                                      keyValuePairs,
+                                                                                      ref resultLevelKind);
+
+                if (currentState != testCase.ExpectedValidationState)
+                {
+                    sb.AppendLine($"The test '{testCase.Title}' was expecting '{testCase.ExpectedValidationState}' but found '{currentState}'.");
+                }
+
+                if (message != testCase.ExpectedMessage)
+                {
+                    sb.AppendLine($"The test '{testCase.Title}' was expecting '{testCase.ExpectedMessage}' but found '{message}'.");
+                }
+
+                mockHandler.Clear();
+            }
+
+            sb.Length.Should().Be(0, sb.ToString());
+        }
+    }
+}
---
+++ Src/Plugins/Tests.Security/TestData/SecurePlaintextSecrets/Inputs/SEC101_017.NpmLegacyAuthorToken.ps1
@@ -0,0 +1,3 @@
+npm: "338a0fd3-7378-4ce1-8432-bbfc0f6777ca"
+
+<npm>338a0fd3-7378-4ce1-8432-bbfc0f6777cb</npm>
