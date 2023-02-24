a8dc0de575e3c357dcde599b224eb386f02b1255
Yong Yan <yanyong-gh@outlook.com>
2023-02-24 14:32:52 -0800

    Re-enable `SEC101/029.AlibabaCloudCredentials` (#711)
    Co-authored-by: Yong Yan <v-yongyan@microsoft.com>

--- Src/Plugins/Security/SecurePlaintextSecretsValidators/SEC101_029.AlibabaCloudCredentialsValidator.cs
+++ Src/Plugins/Security/SecurePlaintextSecretsValidators/SEC101_029.AlibabaCloudCredentialsValidator.cs
@@ -1,23 +1,33 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
 using Microsoft.RE2.Managed;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
 {
-    public class AlibabaCloudCredentialsValidator : StaticValidatorBase
+    public class AlibabaCloudCredentialsValidator : DynamicValidatorBase
     {
-        private const string KeyNotFound = "InvalidAccessKeyId.NotFound";
-        private const string InvalidSecret = "SDK.InvalidAccessKeySecret";
-        private const string ProductKeyInvalidFormat = "The productKey format is incorrect.";
+        internal static string UriTemplate = "https://ecs.aliyuncs.com/";
+
+        internal DateTime? Timestamp { get; set; }
+
+        internal string SignatureNonce { get; set; }
 
         protected override IEnumerable<ValidationResult> IsValidStaticHelper(IDictionary<string, FlexMatch> groups)
         {
             if (!groups.TryGetNonEmptyValue("id", out FlexMatch id) ||
                 !groups.TryGetNonEmptyValue("secret", out FlexMatch secret))
+            {
+                return ValidationResult.CreateNoMatch();
+            }
+
+            if (IsFalsePositiveOrBelongsToOtherSecurityModel(secret.Value))
             {
                 return ValidationResult.CreateNoMatch();
             }
@@ -36,50 +46,68 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
             return new[] { validationResult };
         }
 
-        // protected override ValidationState IsValidDynamicHelper(ref Fingerprint fingerprint,
-        //                                                         ref string message,
-        //                                                         IDictionary<string, string> options,
-        //                                                         ref ResultLevelKind resultLevelKind)
-        // {
-        //     string account = fingerprint.Id;
-        //     string password = fingerprint.Secret;
-        //     try
-        //     {
-        //         // Taken from https://www.alibabacloud.com/help/doc-detail/63638.htm
-        //         IClientProfile clientProfile = DefaultProfile.GetProfile("cn-shanghai", account, password);
-        //         var client = new DefaultAcsClient(clientProfile);
-        //         var request = new PubRequest();
-        //         // We don't actually care about any of the product stuff, so just leave dummy values in.
-        //         request.ProductKey = "<productKey>";
-        //         request.TopicFullName = "/<productKey>/<deviceName>/get";
-        //         byte[] payload = Encoding.Default.GetBytes("Invalid payload.");
-        //         string payloadStr = Convert.ToBase64String(payload);
-        //         request.MessageContent = payloadStr;
-        //         request.Qos = 0;
-        //         PubResponse response = client.GetAcsResponse(request);
-        //         if (!response.ErrorMessage.Equals(ProductKeyInvalidFormat))
-        //         {
-        //             message = $"Unexpected response from Alibaba Cloud: '{response.ErrorMessage}'";
-        //             return ValidationState.Unknown;
-        //         }
-        //     }
-        //     catch (ClientException ce)
-        //     {
-        //         switch (ce.ErrorCode)
-        //         {
-        //             case KeyNotFound:
-        //                 // Not even the client id we found is valid. Return no match.
-        //                 return ValidationState.NoMatch;
-        //             case InvalidSecret:
-        //                 // The client ID is valid but the secret was not.
-        //                 return ReturnUnauthorizedAccess(ref message, asset: account);
-        //             default:
-        //                 return ReturnUnhandledException(ref message, ce, asset: account);
-        //         }
-        //     }
-        //     // If all goes well, we'll receive a "product format invalid" message in the response
-        //     // which means authentication succeeded. Therefore the id and secret are valid.
-        //     return ReturnAuthorizedAccess(ref message, asset: account);
-        // }
+        protected override ValidationState IsValidDynamicHelper(ref Fingerprint fingerprint,
+                                                        ref string message,
+                                                        IDictionary<string, string> options,
+                                                        ref ResultLevelKind resultLevelKind)
+        {
+            // https://www.alibabacloud.com/help/en/log-service/latest/accesskey-pair
+            // https://www.alibabacloud.com/help/en/log-service/latest/request-signatures#t13222.html
+            // https://next.api.alibabacloud.com/product/Ecs#endpoint
+
+            string id = fingerprint.Id;
+            string secret = fingerprint.Secret;
+
+            try
+            {
+                HttpClient client = CreateOrRetrieveCachedHttpClient();
+
+                using var request = new HttpRequestMessage(HttpMethod.Get, UriTemplate);
+
+                var requestSigner = new AlibabaEcsRequestSigner(this.Timestamp, this.SignatureNonce);
+
+                requestSigner.SignRequest(request, id, secret);
+
+                using HttpResponseMessage response = client
+                    .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                    .GetAwaiter()
+                    .GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return ValidationState.Authorized;
+                }
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.Forbidden:
+                    {
+                        // "Forbidden.AccessKeyDisabled"
+                        return ValidationState.Unauthorized;
+                    }
+
+                    case HttpStatusCode.NotFound:
+                    {
+                        // "InvalidAccessKeyId.NotFound"
+                        return ValidationState.Unauthorized;
+                    }
+
+                    case HttpStatusCode.BadRequest:
+                    {
+                        // "IncompleteSignature"
+                        return ValidationState.Unauthorized;
+                    }
+
+                    default:
+                    {
+                        return ReturnUnexpectedResponseCode(ref message, response.StatusCode);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return ReturnUnhandledException(ref message, e);
+            }
+        }
     }
 }
---
+++ Src/Plugins/Tests.Security/SecurePlaintextSecretsValidators/SEC101_029.AlibabaCloudCredentialsValidatorTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+using FluentAssertions;
+
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Helpers;
+using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
+
+using Newtonsoft.Json;
+
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validators
+{
+    /// <summary>
+    /// Testing SEC101/029.AlibabaCloudCredentialsValidator
+    /// </summary>
+    public class AlibabaCloudCredentialsValidatorTests
+    {
+        [Fact]
+        public void AlibabaCloudCredentialsKeyValidator_MockedDynamicAnalysis()
+        {
+            string unknownStatusCodeMessage = null, unhandledExceptionMessage = null;
+
+            var fingerprint = new Fingerprint("[id=LTAIid1][secret=abc]");
+            var expectedFingerprint = new Fingerprint("[id=LTAIid1][secret=abc]");
+            string uri = string.Format(AlibabaCloudCredentialsValidator.UriTemplate);
+
+            DateTime timestamp = DateTime.UtcNow;
+            string nonce = Guid.NewGuid().ToString();
+            using var defaultRequest = new HttpRequestMessage(HttpMethod.Get, uri);
+            var signer = new AlibabaEcsRequestSigner(timestamp, nonce);
+            signer.SignRequest(defaultRequest, fingerprint.Id, fingerprint.Secret);
+
+            ValidatorBase.ReturnUnexpectedResponseCode(ref unknownStatusCodeMessage, HttpStatusCode.InternalServerError);
+            ValidatorBase.ReturnUnhandledException(ref unhandledExceptionMessage, new NullReferenceException());
+
+            var okResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("{\"RequestId\":\"7D066872-0FE5-5BC6-9A17-FF7D5A7479B4\",\"Regions\":{\"Region\":[{\"RegionId\":\"us-east-1\",\"RegionEndpoint\":\"ecs.us-east-1.aliyuncs.com\",\"LocalName\":\"美国（弗吉尼亚）\"}]}}")
+            };
+
+            var testCases = new HttpMockTestCase[]
+            {
+                new  HttpMockTestCase
+                {
+                    Title = "Auth request returns HttpStatusCode.OK",
+                    ExpectedValidationState = ValidationState.Authorized,
+                    HttpRequestMessages = new List<HttpRequestMessage> { defaultRequest },
+                    HttpResponseMessages = new [] { okResponse },
+                    ExpectedMessage = string.Empty,
+                    ExpectedFingerprint = expectedFingerprint,
+                },
+                new  HttpMockTestCase
+                {
+                    Title = "Auth request returns HttpStatusCode.Forbidden",
+                    ExpectedValidationState = ValidationState.Unauthorized,
+                    HttpRequestMessages = new List<HttpRequestMessage> { defaultRequest },
+                    HttpResponseMessages = new [] { HttpMockHelper.ForbiddenResponse },
+                    ExpectedMessage = string.Empty,
+                    ExpectedFingerprint = expectedFingerprint,
+                },
+                new  HttpMockTestCase
+                {
+                    Title = "Auth request returns HttpStatusCode.BadRequest",
+                    HttpRequestMessages = new List<HttpRequestMessage> { defaultRequest },
+                    HttpResponseMessages = new [] { HttpMockHelper.BadRequestResponse },
+                    ExpectedValidationState = ValidationState.Unauthorized,
+                    ExpectedMessage = string.Empty,
+                    ExpectedFingerprint = expectedFingerprint,
+                },
+                new  HttpMockTestCase
+                {
+                    Title = "Auth request returns HttpStatusCode.NotFound",
+                    HttpRequestMessages = new List<HttpRequestMessage> { defaultRequest },
+                    HttpResponseMessages = new [] { HttpMockHelper.NotFoundResponse },
+                    ExpectedValidationState = ValidationState.Unauthorized,
+                    ExpectedMessage = string.Empty,
+                    ExpectedFingerprint = expectedFingerprint,
+                },
+                new  HttpMockTestCase
+                {
+                    Title = "Unexpected Response Code (500 internal server error)",
+                    HttpRequestMessages = new List<HttpRequestMessage> { defaultRequest },
+                    HttpResponseMessages = new [] { HttpMockHelper.InternalServerErrorResponse },
+                    ExpectedValidationState = ValidationState.Unknown,
+                    ExpectedMessage = unknownStatusCodeMessage,
+                },
+                new  HttpMockTestCase
+                {
+                    Title = "Null Reference Exception",
+                    HttpRequestMessages = new List<HttpRequestMessage> { null },
+                    HttpResponseMessages = new List<HttpResponseMessage> { null },
+                    ExpectedValidationState = ValidationState.Unknown,
+                    ExpectedMessage = unhandledExceptionMessage
+                }
+            };
+
+            var sb = new StringBuilder();
+            var mockHandler = new HttpMockHelper();
+            var validator = new AlibabaCloudCredentialsValidator
+            {
+                Timestamp = timestamp,
+                SignatureNonce = nonce,
+            };
+
+            foreach (HttpMockTestCase testCase in testCases)
+            {
+                for (int i = 0; i < testCase.HttpRequestMessages.Count; i++)
+                {
+                    mockHandler.Mock(testCase.HttpRequestMessages[i], testCase.HttpResponseMessages[i]);
+                }
+
+                string message = string.Empty;
+                ResultLevelKind resultLevelKind = default;
+                var keyValuePairs = new Dictionary<string, string>();
+
+                using var httpClient = new HttpClient(mockHandler);
+                validator.SetHttpClient(httpClient);
+
+                ValidationState currentState = validator.IsValidDynamic(ref fingerprint,
+                                                                        ref message,
+                                                                        keyValuePairs,
+                                                                        ref resultLevelKind);
+                if (currentState != testCase.ExpectedValidationState)
+                {
+                    sb.AppendLine($"The test case '{testCase.Title}' was expecting '{testCase.ExpectedValidationState}' but found '{currentState}'.");
+                }
+
+                if (testCase.ExpectedMessage != message?.Split(Environment.NewLine)[0])
+                {
+                    sb.AppendLine($"The test case '{testCase.Title}' was expecting '{testCase.ExpectedMessage}' but found '{message}'.");
+                }
+
+                mockHandler.Clear();
+            }
+
+            sb.Length.Should().Be(0, sb.ToString());
+        }
+    }
+}
