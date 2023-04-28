3e6e60ad2d6f480cad3a00504898958f1be135ff
Ling Zhou <v-zhouling@microsoft.com>
2023-04-27 23:04:01 -0700

    Expand the Crc32 class by adding support for Castagnoli's polynomial without affecting the existing public methods and their reference. The default crc32 polynomial  will be IEEE polynomial.

--- Src/Sarif.PatternMatcher.Sdk/Crc32.cs
+++ Src/Sarif.PatternMatcher.Sdk/Crc32.cs
@@ -13,9 +13,17 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk
         // This is the 'reversed representation' polynomial for
         // little-endian implementations, i.e., the bitwise
         // reflection of 0x04C11DB7;
-        private const uint Crc32Polynomial = 0xedb88320u;
+        // CRC-32-IEEE 802.3 polynomial
+        private const uint Crc32IEEEPolynomial = 0xedb88320u;
 
-        private static readonly uint[] Crc32Table = CreateCrcTable(Crc32Polynomial);
+        // Castagnoli's polynomial, used in iSCSI.
+        // Has better error detection characteristics than IEEE.
+        // https://dx.doi.org/10.1109/26.231911
+        private const uint Crc32CastagnoliPolynomial = 0x82f63b78;
+
+        public static readonly uint[] Crc32IEEETable = CreateCrcTable(Crc32IEEEPolynomial);
+
+        public static readonly uint[] Crc32CastagnoliTable = CreateCrcTable(Crc32CastagnoliPolynomial);
 
         public static uint Calculate(string text)
         {
@@ -30,29 +38,45 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk
 
         public static uint Calculate(uint checksum, byte[] buffer, int offset, int length)
         {
+            return Calculate(checksum, buffer, offset, length, Crc32IEEETable);
+        }
+
+        public static uint Calculate(string text, uint[] crc32Table)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            return Calculate(data, crc32Table);
+        }
+
+        public static uint Calculate(byte[] buffer, uint[] crc32Table)
+        {
+            return Calculate(0, buffer, 0, buffer.Length, crc32Table);
+        }
+
+        public static uint Calculate(uint checksum, byte[] buffer, int offset, int length, uint[] crc32Table)
+        {
             checksum ^= 0xffffffffU;
 
             while (length >= 16)
             {
-                uint a = Crc32Table[(3 * 256) + buffer[offset + 12]] ^
-                         Crc32Table[(2 * 256) + buffer[offset + 13]] ^
-                         Crc32Table[(1 * 256) + buffer[offset + 14]] ^
-                         Crc32Table[(0 * 256) + buffer[offset + 15]];
+                uint a = crc32Table[(3 * 256) + buffer[offset + 12]] ^
+                         crc32Table[(2 * 256) + buffer[offset + 13]] ^
+                         crc32Table[(1 * 256) + buffer[offset + 14]] ^
+                         crc32Table[(0 * 256) + buffer[offset + 15]];
 
-                uint b = Crc32Table[(7 * 256) + buffer[offset + 8]] ^
-                         Crc32Table[(6 * 256) + buffer[offset + 9]] ^
-                         Crc32Table[(5 * 256) + buffer[offset + 10]] ^
-                         Crc32Table[(4 * 256) + buffer[offset + 11]];
+                uint b = crc32Table[(7 * 256) + buffer[offset + 8]] ^
+                         crc32Table[(6 * 256) + buffer[offset + 9]] ^
+                         crc32Table[(5 * 256) + buffer[offset + 10]] ^
+                         crc32Table[(4 * 256) + buffer[offset + 11]];
 
-                uint c = Crc32Table[(11 * 256) + buffer[offset + 4]] ^
-                         Crc32Table[(10 * 256) + buffer[offset + 5]] ^
-                         Crc32Table[(9 * 256) + buffer[offset + 6]] ^
-                         Crc32Table[(8 * 256) + buffer[offset + 7]];
+                uint c = crc32Table[(11 * 256) + buffer[offset + 4]] ^
+                         crc32Table[(10 * 256) + buffer[offset + 5]] ^
+                         crc32Table[(9 * 256) + buffer[offset + 6]] ^
+                         crc32Table[(8 * 256) + buffer[offset + 7]];
 
-                uint d = Crc32Table[(15 * 256) + ((byte)checksum ^ buffer[offset])] ^
-                         Crc32Table[(14 * 256) + ((byte)(checksum >> 8) ^ buffer[offset + 1])] ^
-                         Crc32Table[(13 * 256) + ((byte)(checksum >> 16) ^ buffer[offset + 2])] ^
-                         Crc32Table[(12 * 256) + ((checksum >> 24) ^ buffer[offset + 3])];
+                uint d = crc32Table[(15 * 256) + ((byte)checksum ^ buffer[offset])] ^
+                         crc32Table[(14 * 256) + ((byte)(checksum >> 8) ^ buffer[offset + 1])] ^
+                         crc32Table[(13 * 256) + ((byte)(checksum >> 16) ^ buffer[offset + 2])] ^
+                         crc32Table[(12 * 256) + ((checksum >> 24) ^ buffer[offset + 3])];
 
                 checksum = d ^ c ^ b ^ a;
                 offset += 16;
@@ -61,7 +85,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk
 
             while (--length >= 0)
             {
-                checksum = Crc32Table[(checksum ^ buffer[offset++]) & 0xFF] ^ (checksum >> 8);
+                checksum = crc32Table[(checksum ^ buffer[offset++]) & 0xFF] ^ (checksum >> 8);
             }
 
             checksum ^= 0xffffffffU;
