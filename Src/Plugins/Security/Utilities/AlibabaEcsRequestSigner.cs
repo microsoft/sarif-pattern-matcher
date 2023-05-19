42d63408bd3ab8bf31af44f01f10a7a5aeafb518
Craig Schlaman <v-crschlaman@microsoft.com>
2023-05-19 15:21:31 -0700

    Make XML parameter comment a little more complete.

--- Src/Plugins/Security/Utilities/AlibabaEcsRequestSigner.cs
+++ Src/Plugins/Security/Utilities/AlibabaEcsRequestSigner.cs
@@ -39,7 +39,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
         /// Sign the HTTP request using Alibaba Cloud access key and secret so it
         /// can be used to access Alibaba Cloud service REST API.
         /// </summary>
-        /// <param name="request">httpRequest.</param>
+        /// <param name="request">Unsigned httpRequest passed in that will be signed for Alibaba Cloud service REST API.</param>
         /// <param name="accessKey">Alibbaba Cloud access key.</param>
         /// <param name="secret">Alibaba Cloud secret.</param>
         public void SignRequest(HttpRequestMessage request, string accessKey, string secret)
