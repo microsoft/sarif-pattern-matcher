870159f5591818b405f2f041ca23a516fceaa1c2
Yong Yan <yanyong-gh@outlook.com>
2023-06-21 15:22:14 -0700

    Remove `Octokit` dependencies and refactor the GitHub Pat validator to use HTTP request instead

--- Src/Plugins/Tests.Security/SecurePlaintextSecretsValidators/SEC101_006.GitHubLegacyPatValidatorTests.cs
+++ Src/Plugins/Tests.Security/SecurePlaintextSecretsValidators/SEC101_006.GitHubLegacyPatValidatorTests.cs
@@ -2,11 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 using Microsoft.CodeAnalysis.Sarif.PatternMatcher.Sdk;
 using Microsoft.RE2.Managed;
 
 using Xunit;
+using FluentAssertions;
 
 namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validators
 {
@@ -57,6 +62,58 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validator
                                                                                       ref resultLevelKind);
 
             Assert.Equal(expectedValidationState, actualValidationState);
+        }
+
+        [Fact]
+        public void GitHubLegacyPatValidator_ValidatePat_ShouldReturnAuthorizedWhenPatIsLive()
+        {
+            const string id = "123";
+            const string login = "login";
+            const string userName = "userName";
+
+            var sb = new StringBuilder();
+            var validator = new GitHubLegacyPatValidator();
+            var fingerprint = new Fingerprint { Secret = "secret" };
+            string uri = "https://api.github.com/user";
+
+            var mockHandler = new HttpMockHelper();
+
+            using var defaultRequest = new HttpRequestMessage(HttpMethod.Get, uri);
+            defaultRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "secret");
+
+            var okResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(
+                    $@"{{
+                        ""id"":""{id}"",
+                        ""login"":""{login}"",
+                        ""name"":""{userName}"",
+                        ""node_id"":""abcd1234""
+                    }}
+                ")
+            };
+
+            mockHandler.Mock(defaultRequest, okResponse);
+            using var httpClient = new HttpClient(mockHandler);
+            validator.SetHttpClient(httpClient);
+
+            string message = string.Empty;
+            ResultLevelKind resultLevelKind = default;
+            var keyValuePairs = new Dictionary<string, string>();
+            var actualFingerprint = new Fingerprint(fingerprint.ToString());
+
+            ValidationState state = validator.IsValidDynamic(
+                ref actualFingerprint,
+                ref message,
+                keyValuePairs,
+                ref resultLevelKind);
+
+            if (state != ValidationState.Authorized)
+            {
+                sb.AppendLine($"The test was expecting '{nameof(ValidationState.Authorized)}' but found '{state}' for '{uri}'.");
+            }
+
+            sb.Length.Should().Be(0, sb.ToString());
         }
     }
 }
