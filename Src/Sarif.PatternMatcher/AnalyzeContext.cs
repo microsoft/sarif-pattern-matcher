872caaa6261118e01ef39d10114f7d34c8f75bbd
Michael C. Fanning <mikefan@microsoft.com>
2023-06-26 17:37:41 -0700

    Make IronRE2 the default for linux.

--- Src/Sarif.PatternMatcher/AnalyzeContext.cs
+++ Src/Sarif.PatternMatcher/AnalyzeContext.cs
@@ -113,8 +113,8 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
 
         public static PerLanguageOption<RegexEngine> RegexEngineProperty =>
             new PerLanguageOption<RegexEngine>(
-                "CoreSettings", nameof(RegexEngine), defaultValue: () => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? RegexEngine.RE2 : RegexEngine.CachedDotNet,
-                "The pattern matching to use for scanning. One of RE2 (Windows default), DotNet, CachedDotNet (Linux default) or IronRE2.");
+                "CoreSettings", nameof(RegexEngine), defaultValue: () => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? RegexEngine.RE2 : RegexEngine.IronRE2,
+                "The pattern matching to use for scanning. One of RE2 (Windows default), DotNet, CachedDotNet or IronRE2 (Linux default).");
 
         public static PerLanguageOption<string> SniffRegexProperty =>
             new PerLanguageOption<string>(
