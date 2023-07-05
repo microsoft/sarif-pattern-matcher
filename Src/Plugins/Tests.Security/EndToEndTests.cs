b00d75885f89fdde7aaccd1cbbdb3c89ab25b101
Shaopeng Li <v-shaopengli@microsoft.com>
2023-07-04 21:49:42 -0700

    Src

--- Src/Plugins/Tests.Security/EndToEndTests.cs
+++ Src/Plugins/Tests.Security/EndToEndTests.cs
@@ -58,7 +58,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
 
         protected override string TestLogResourceNameRoot => $"Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.TestData.{TypeUnderTest}";
 
-        protected override string TestBinaryTestDataDirectory => Path.Combine(ProductRootDirectory, "src", "Plugins", TestBinaryName, "TestData");
+        protected override string TestBinaryTestDataDirectory => Path.Combine(ProductRootDirectory, "Src", "Plugins", TestBinaryName, "TestData");
 
         protected override string ProductTestDataDirectory => Path.Combine(TestBinaryTestDataDirectory, TypeUnderTest);
 
