c65c1b605bd86b71ed4585c21e9ae8cbd6747323
Shaopeng Li <v-shaopengli@microsoft.com>
2023-07-10 15:07:57 -0700

    fix disable AnalyzeCommand_AnalyzeFromContext_CancelledExternallyMultithreaded

--- Src/Test.UnitTests.Sarif.PatternMatcher/AnalyzeCommandTests.cs
+++ Src/Test.UnitTests.Sarif.PatternMatcher/AnalyzeCommandTests.cs
@@ -275,7 +275,6 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher
             }
         }
 
-        [Fact]
         [Fact(Skip = "Random fails.")]
         public void AnalyzeCommand_AnalyzeFromContext_CancelledExternallyMultithreaded()
         {
