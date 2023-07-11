aa5f2f68d880f38bc983c2b4050de27827a29091
Shaopeng Li <v-shaopengli@microsoft.com>
2023-07-11 11:11:21 -0700

    remove # Reset the path from Sarif.Sdk back to SarifPatternMatcher.     $RepoRoot = $(Resolve-Path $PSScriptRoot\..).Path

--- scripts/BuildAndTest.ps1
+++ scripts/BuildAndTest.ps1
@@ -64,8 +64,6 @@ if (-not $NoBuild) {
         Exit-WithFailureMessage $ScriptName "Build of sarif.sdk failed."	
     }
 
-    $RepoRoot = $(Resolve-Path $PSScriptRoot\..).Path
-
     Write-Information "Building SarifPatternMatcher.sln (dotnet)..."
     dotnet build $RepoRoot\src\SarifPatternMatcher.sln -c $Configuration -p:Deterministic=true
     if ($LASTEXITCODE -ne 0) {
