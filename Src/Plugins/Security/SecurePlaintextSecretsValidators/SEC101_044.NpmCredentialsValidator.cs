b16822c7494900d7e0e9805cc87e05082050921f
Ling Zhou <v-zhouling@microsoft.com>
2023-07-27 11:55:41 -0700

    more message update

--- Src/Plugins/Security/SecurePlaintextSecretsValidators/SEC101_044.NpmCredentialsValidator.cs
+++ Src/Plugins/Security/SecurePlaintextSecretsValidators/SEC101_044.NpmCredentialsValidator.cs
@@ -101,7 +101,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security
                     responseWithNoCredentials.StatusCode == HttpStatusCode.NotFound ||
                     responseWithNoCredentials.StatusCode == HttpStatusCode.NonAuthoritativeInformation)
                 {
-                    message = $"The Uri {uri} is generic, no way to validate the credential with for account {id}";
+                    message = $"There is no way to validate the credential against the Uri {uri} for account {id}";
 
                     return ValidationState.Unknown;
                 }
--- Src/Plugins/Tests.Security/SecurePlaintextSecretsValidators/SEC101_044.NpmCredentialsValidatorTests.cs
+++ Src/Plugins/Tests.Security/SecurePlaintextSecretsValidators/SEC101_044.NpmCredentialsValidatorTests.cs
@@ -30,7 +30,7 @@ namespace Microsoft.CodeAnalysis.Sarif.PatternMatcher.Plugins.Security.Validator
             const string secret = "secret";
             string uri = $"https://{host}";
             using var requestWithNoCredentials = new HttpRequestMessage(HttpMethod.Get, uri);
-            const string unknownMessage = "The Uri https://host is generic, no way to validate the credential with for account id";
+            const string unknownMessage = "There is no way to validate the credential against the Uri https://host for account id";
 
             string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", id, secret)));
 
